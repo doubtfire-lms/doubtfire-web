867488b7735a1f6fd13a976bac8ffd31441d3356
John Stableford <stableford.john@gmail.com>
2016-08-12 15:26:18 +1000

    CONFIG: fix docker environment and bump moment version

--- config/environment/docker.env.coffee
+++ config/environment/docker.env.coffee
@@ -1,4 +1,4 @@
 module.exports = {
-  API_URL: 'http://' + process.env.DOUBTFIRE_DOCKER_MACHINE_IP + ':3000/api'
+  API_URL: 'http://COPYENV_REPLACE_WITH_DOUBTFIRE_DOCKER_MACHINE_IP:3000/api'
   DEV_MODE: 'true'
 }
--- config/gulp/copyenv.js
+++ config/gulp/copyenv.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 
 var NODE_ENV = process.env.NODE_ENV;
 
 gulp.task('copyenv', function() {
   return gulp.src('config/environment/' + (NODE_ENV || 'development') + '.env.coffee')
   .pipe(rename('env.coffee'))
+  .pipe(replace(/COPYENV_REPLACE_WITH_DOUBTFIRE_DOCKER_MACHINE_IP/, process.env.DOUBTFIRE_DOCKER_MACHINE_IP))
   .pipe(gulp.dest('src/app/api'));
 });
