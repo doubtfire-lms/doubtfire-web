f16fa5fb56630e257f35fad4d73a4ccbb6a0d922
Jake Renzella <j@kerenzella.com>
2018-11-02 16:30:40 +1100

    QUALITY: Add new constants ts file

--- src/app/config/constants/constants.ts
+++ src/app/config/constants/constants.ts
@@ -1,12 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface externalNameResponseFormat {
+  externalName: String;
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class DoubtfireConstants {
-  public static mainContributors: Array<string> = [
-      'macite',              // Andrew Cain
-      'alexcu',              // Alex Cummaudo
-      'jakerenzella'
-    ];
+  constructor(private httpClient: HttpClient) {
+    console.log("getting api url");
+    httpClient.get(`${this.apiURL}/settings`).toPromise().then((response: externalNameResponseFormat) => {
+      this.externalName = response.externalName;
+      console.log("Loaded " + response.externalName);
+    })
+  }
+  public mainContributors: Array<string> = [
+    'macite',              // Andrew Cain
+    'alexcu',              // Alex Cummaudo
+    'jakerenzella'
+  ];
+  public apiURL = "http://localhost:3000/api"
+  public externalName: String = "Loading...";
 }
+
