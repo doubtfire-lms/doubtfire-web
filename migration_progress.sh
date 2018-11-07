4ff91a600f0d93bf944383358bdd66d363f61389
Jake Renzella <j@kerenzella.com>
2018-11-08 10:35:38 +1100

    ENHANCE: Add markdown checkboxes to migration progress output

--- migration_progress.sh
+++ migration_progress.sh
@@ -1,12 +1,12 @@
-function count_to_migrate() { grep -Ril --include \*.coffee "angular.module" ./src/app; }
-function count_migrated() { grep -Ril --include \*.ts "@Component" ./src/app; }
+function count_to_migrate() { grep -Ril --include \*.coffee "angular.module" ./src/app | sed 's/^/- [ ] /'; }
+function count_migrated() { grep -Ril --include \*.ts "@Component" ./src/app | sed 's/^/- [x] /';}
 function print_progress() { echo $1 / $2 components migrated; }
 
-echo "\nTODO:\n"
-count_to_migrate
+echo "\nSUMMARY:\n"
+print_progress `count_migrated | wc -l` `count_to_migrate | wc -l`
 
 echo "\nMIGRATED:\n"
 count_migrated
 
-echo "\nSUMMARY:\n"
-print_progress `count_migrated | wc -l` `count_to_migrate | wc -l`
+echo "\nTODO:\n"
+count_to_migrate
