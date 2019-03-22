261f57be7d7dff0a213ad2af3cfd3757cfefe9d1
Jake Renzella <j@kerenzella.com>
2019-03-22 19:32:50 +1100

    ENHANCE: Remove log and fix type of array

--- src/app/config/constants/constants.ts
+++ src/app/config/constants/constants.ts
@@ -11,15 +11,14 @@ interface externalNameResponseFormat {
 })
 export class DoubtfireConstants {
   constructor(private httpClient: HttpClient) {
-    console.log("getting api url");
     httpClient.get(`${this.apiURL}/settings`).toPromise().then((response: externalNameResponseFormat) => {
       this.externalName = response.externalName;
     })
   }
-  public mainContributors: Array<string> = [
+  public mainContributors: string[] = [
     'macite',              // Andrew Cain
     'alexcu',              // Alex Cummaudo
-    'jakerenzella'
+    'jakerenzella'         // Jake Renzella
   ];
   public apiURL = apiURL;
   public externalName: String = "Loading...";
