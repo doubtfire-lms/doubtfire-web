7168e0476e6fd01294413d52b62c3777df4b3f3f
Jake Renzella <j@kerenzella.com>
2019-04-09 15:05:51 +1000

    QUALITY: Remove unused HttpErrorResponse import

--- src/app/config/constants/doubtfire-constants.ts
+++ src/app/config/constants/doubtfire-constants.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
