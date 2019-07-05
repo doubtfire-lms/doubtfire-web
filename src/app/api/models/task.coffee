9a62cb197e5a8c7f8882c3eb8c7dcd6b01cf8678
Jake Renzella <j@kerenzella.com>
2019-07-05 16:11:05 +1000

    NEW: Add new extension request

--- src/app/api/models/task.coffee
+++ src/app/api/models/task.coffee
@@ -8,7 +8,7 @@ angular.module("doubtfire.api.models.task", [])
   Task.summaryData = resourcePlus "/tasks/:id", { id: "@id" }
 
   Task.applyForExtension = (task, reason, weeksRequested, onSuccess, onError) ->
-    $http.post("#{DoubtfireConstants.API_URL}/projects/#{task.project().project_id}/task_def_id/#{task.task_definition_id}/extension", { comment: reason, weeks_requested: weeksRequested }).then(
+    $http.post("#{DoubtfireConstants.API_URL}/projects/#{task.project().project_id}/task_def_id/#{task.task_definition_id}/request_extension", { comment: reason, weeks_requested: weeksRequested }).then(
       (data) -> onSuccess(data)
       (response) -> onError(response)
     )
--- src/app/common/modals/extension-modal/extension-modal.component.ts
+++ src/app/common/modals/extension-modal/extension-modal.component.ts
@@ -1,24 +1,39 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { alertService } from 'src/app/ajs-upgraded-providers';
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-extension-modal',
+  selector: 'extension-modal',
   templateUrl: './extension-modal.component.html',
   styleUrls: ['./extension-modal.component.scss']
 })
 export class ExtensionModalComponent implements OnInit {
-  weeksRequested: number = 1;
+  weeksRequested: number;
   reason: string = '';
   constructor(public dialogRef: MatDialogRef<ExtensionModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     @Inject(alertService) private alerts: any, ) { }
 
   ngOnInit() {
+    this.weeksRequested = this.minWeeksCanExtend + 1;
   }
 
-  get weeksCanExtend() {
-    return this.data.task.weeksCanExtend();
+  get newDueDate() {
+    const calculatedDueDate = moment(this.data.task.targetDate()).add(this.weeksRequested, 'weeks');
+    const taskDueDate = moment(this.data.task.definition.due_date);
+    if (calculatedDueDate > taskDueDate) {
+      return taskDueDate.format('DD of MMMM');
+    }
+    return calculatedDueDate.format('DD of MMMM');
+  }
+
+  get maxWeeksCanExtend() {
+    return this.data.task.maxWeeksCanExtend();
+  }
+
+  get minWeeksCanExtend() {
+    return this.data.task.minWeeksCanExtend();
   }
 
   submitApplication() {
