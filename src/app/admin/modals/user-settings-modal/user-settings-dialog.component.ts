8c62e0260b0623aa3d230b1686eea1592f1d7a5a
Reuben Wilson <reubenmwilson@gmail.com>
2019-09-24 11:56:54 +1000

    QUALITY: Extend on migration

--- src/app/admin/modals/user-settings-modal/user-settings-dialog.component.ts
+++ src/app/admin/modals/user-settings-modal/user-settings-dialog.component.ts
@@ -4,15 +4,14 @@
 import { Injectable, Component, Inject } from '@angular/core';
 import { UserSettingsDialogService } from '../user-settings-modal/user-settings-dialog.service';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatRadioModule } from '@angular/material';
 import { DoubtfireConstants } from 'src/app/config/constants/doubtfire-constants';
-import { currentUser } from 'src/app/ajs-upgraded-providers';
+import { currentUser, auth, analyticsService } from 'src/app/ajs-upgraded-providers';
 
 interface UserSettingsDialogData {
   user: any,
-  currentUser: any,
   externalName: string,
   isNew: boolean,
+  service: any
 }
 
 @Component({
@@ -20,7 +19,27 @@ interface UserSettingsDialogData {
   templateUrl: 'user-settings-dialog.component.html',
 })
 export class UserSettingsDialogContent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: UserSettingsDialogData) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: UserSettingsDialogData, @Inject(auth) private auth: any,
+    @Inject(analyticsService) private analyticsService: any, @Inject(currentUser) private currentUser: any) { }
+
+  private createNewUser() {
+    this.data.service.createUser(this.data.user).subscribe(result => { });
+  }
+
+  private updateExistingUser() {
+    this.data.service.updateUser(this.data.user).subscribe(result => {
+      this.data.user.name = `${this.data.user.first_name} ${this.data.user.last_name}`;
+      if (this.data.user == this.currentUser.profile) {
+        this.auth.saveCurrentUser();
+        if (this.data.user.opt_in_to_research)
+          this.analyticsService.event("Doubtfire Analytics", "User opted in research");
+      }
+    });
+  }
+
+  saveUser() {
+    this.data.isNew ? this.createNewUser() : this.updateExistingUser();
+  }
 }
 
 @Injectable()
@@ -29,14 +48,13 @@ export class UserSettingsDialog {
   userSettingsDialogData: UserSettingsDialogData;
 
   constructor(public dialog: MatDialog,
-    @Inject(currentUser) private currentUser: any,
     private constants: DoubtfireConstants,
     private userSettingsDialogServive: UserSettingsDialogService) {
     this.userSettingsDialogData = {
       externalName: '',
       user: {},
-      currentUser: currentUser,
-      isNew: false
+      isNew: false,
+      service: userSettingsDialogServive,
     }
   }
 
--- src/app/admin/modals/user-settings-modal/user-settings-dialog.service.ts
+++ src/app/admin/modals/user-settings-modal/user-settings-dialog.service.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { User } from 'src/app/ajs-upgraded-providers';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class UserSettingsDialogService {
-  constructor() { }
+  constructor(@Inject(User) private user: any, ) { }
+
+  createUser(newUser: any): Observable<Object> {
+    return Observable.create(
+      observer => this.user.create(newUser, (response: any) => { observer.next(response) }, (error: any) => { })
+    )
+  }
+
+  updateUser(user: any): Observable<Object> {
+    return Observable.create(
+      observer => this.user.update({id: user.id, user: user}, (response: any) => { observer.next(response) }, (error: any) => { console.log(error) })
+    )
+  }
 }
+
+
+// this.ts.postDiscussionReply(task, taskCommentID, audio, () => { }, (error: any) => this.handleError(error));
