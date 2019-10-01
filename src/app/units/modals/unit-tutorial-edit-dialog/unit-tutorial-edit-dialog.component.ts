f7a193490e66d0bc9520ae460c0863824da9aaee
Reuben Wilson <reubenmwilson@gmail.com>
2019-10-01 13:34:34 +1000

    QUALITY: Remove commented section

--- src/app/units/modals/unit-tutorial-edit-dialog/unit-tutorial-edit-dialog.component.ts
+++ src/app/units/modals/unit-tutorial-edit-dialog/unit-tutorial-edit-dialog.component.ts
@@ -44,51 +44,13 @@ export class UnitTutorialEditDialogContent {
         },
         error => this.alerts.add('danger', `Error creating user. ${(error != null ? error : undefined)}`, 2000));
     } else {
-      this.tutorialService.update(this.data.tutorial).subscribe(
+      this.tutorialService.update(this.data.model.tutorial).subscribe(
         result => {
           this.alerts.add('success', `Updated tutorial`, 2000);
         },
         error => this.alerts.add('danger', `Error creating user. ${(error != null ? error : undefined)}`, 2000));
     }
   }
-
-  // $scope.saveTutorial = ->
-//     save_data = _.omit($scope.tutorial, 'tutor', 'tutor_name', 'meeting_time', 'data')
-//     save_data.tutor_id = if $scope.tutorial.tutor.user_id then $scope.tutorial.tutor.user_id else $scope.tutorial.tutor.id
-
-//     if $scope.tutorial.meeting_time.getHours
-//       save_data.meeting_time = $scope.tutorial.meeting_time.getHours() + ":" + $scope.tutorial.meeting_time.getMinutes()
-
-//     unless save_data.tutor_id?
-//       alertService.add 'danger', 'Ensure that you select a tutor from those engaged in this unit.', 6000
-//       return
-
-//     if $scope.isNew
-//       save_data.unit_id = unit.id
-//       Tutorial.create({ tutorial: save_data }).$promise.then (
-//         (response) ->
-//           $modalInstance.close(response)
-//           $scope.unit.tutorials.push(response)
-//           alertService.add("success", "Tutorial Added", 2000)
-//       ),
-//       (
-//         (response) ->
-//           if response.data.error?
-//             alertService.add("danger", "Error: " + response.data.error, 6000)
-//       )
-//     else
-//       Tutorial.update( { id: tutorial.id, tutorial: save_data } ).$promise.then (
-//         (response) ->
-//           $modalInstance.close(response)
-//           $scope.tutorial.tutor = response.tutor
-//           $scope.tutorial.tutor_name = response.tutor_name
-//           alertService.add("success", "Tutorial Updated", 2000)
-//       ),
-//       (
-//         (response) ->
-//           if response.data.error?
-//             alertService.add("danger", "Error: " + response.data.error, 6000)
-//       )
 }
 
 @Injectable()
@@ -153,63 +115,4 @@ export class UnitTutorialEditDialog {
     return tutor ? tutor.name : undefined;
   }
 }
-
-// d = new Date()
-//   d.setHours(8)
-//   d.setMinutes(30)
-
-//   # Prototype tutorial
-//   prototypeTutorial =
-//     meeting_day: "Monday"
-//     meeting_time: d
-//     abbreviation: null
-//     meeting_location: null
-
-//   $scope.tutorial = tutorial or prototypeTutorial
-//   $scope.isNew = !tutorial?
-//   $scope.unit = unit
-
-//   $scope.tutors = $scope.unit.staff
-
-//   $scope.validForm = () ->
-//     $scope.tutorial.tutor? && $scope.tutorial.abbreviation? && $scope.tutorial.meeting_location?
-
-//   $scope.saveTutorial = ->
-//     save_data = _.omit($scope.tutorial, 'tutor', 'tutor_name', 'meeting_time', 'data')
-//     save_data.tutor_id = if $scope.tutorial.tutor.user_id then $scope.tutorial.tutor.user_id else $scope.tutorial.tutor.id
-
-//     if $scope.tutorial.meeting_time.getHours
-//       save_data.meeting_time = $scope.tutorial.meeting_time.getHours() + ":" + $scope.tutorial.meeting_time.getMinutes()
-
-//     unless save_data.tutor_id?
-//       alertService.add 'danger', 'Ensure that you select a tutor from those engaged in this unit.', 6000
-//       return
-
-//     if $scope.isNew
-//       save_data.unit_id = unit.id
-//       Tutorial.create({ tutorial: save_data }).$promise.then (
-//         (response) ->
-//           $modalInstance.close(response)
-//           $scope.unit.tutorials.push(response)
-//           alertService.add("success", "Tutorial Added", 2000)
-//       ),
-//       (
-//         (response) ->
-//           if response.data.error?
-//             alertService.add("danger", "Error: " + response.data.error, 6000)
-//       )
-//     else
-//       Tutorial.update( { id: tutorial.id, tutorial: save_data } ).$promise.then (
-//         (response) ->
-//           $modalInstance.close(response)
-//           $scope.tutorial.tutor = response.tutor
-//           $scope.tutorial.tutor_name = response.tutor_name
-//           alertService.add("success", "Tutorial Updated", 2000)
-//       ),
-//       (
-//         (response) ->
-//           if response.data.error?
-//             alertService.add("danger", "Error: " + response.data.error, 6000)
-//       )
-// )
 
