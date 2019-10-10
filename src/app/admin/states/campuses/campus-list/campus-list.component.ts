48f89c8d7e168e41b4ad99eacdf4e65cdacda7a5
Reuben Wilson <reubenmwilson@gmail.com>
2019-10-10 13:28:33 +1100

    QUALITY: Enable pushing values to test data

--- src/app/admin/states/campuses/campus-list/campus-list.component.ts
+++ src/app/admin/states/campuses/campus-list/campus-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input, Inject, ViewChild } from '@angular/core';
 import { currentUser, alertService } from 'src/app/ajs-upgraded-providers';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
+import { FormControl } from '@angular/forms';
 
 let ELEMENT_DATA = [
-  { id: 1, abbreviation: 'ABBR-01', name: 'Waterfront', mode: 'Timetable' },
-  { id: 2, abbreviation: 'ABBR-01', name: 'Cloud', mode: 'Automatic' }
+  { abbreviation: 'ABBR-01', name: 'Waterfront', mode: 'Timetable' },
+  { abbreviation: 'ABBR-01', name: 'Cloud', mode: 'Automatic' }
 ];
 
 @Component({
@@ -13,43 +14,61 @@ let ELEMENT_DATA = [
   templateUrl: 'campus-list.component.html',
   styleUrls: ['campus-list.component.scss']
 })
-export class CampusListComponent implements OnInit {
-
-  syncModes = ['Timetable', 'Automatic', 'None'];
-  edit: number;
-
-  displayedColumns: string[] = ['abbreviation', 'name', 'mode', 'options'];
+export class CampusListComponent {
+  @ViewChild(MatTable, { static: true }) table: MatTable<any>;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  // Form controls
+  abbreviation = new FormControl('');
+  name = new FormControl('');
+  sync = new FormControl('');
+  // Set up the table
+  columns: string[] = ['abbreviation', 'name', 'mode', 'options'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  syncModes = ['Timetable', 'Automatic', 'None'];
+  edit: any;
+
+
 
   constructor(
     @Inject(currentUser) private currentUser: any,
     @Inject(alertService) private alertService: any
   ) { }
 
-  ngOnInit() {
-    this.dataSource.sort = this.sort;
+  clearFormControls() {
+    this.abbreviation.setValue('');
+    this.name.setValue('');
+    this.sync.setValue('');
   }
+
+  // ngOnInit() {
+  //   this.data.sort = this.sort;
+  // }
 
   applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    // this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  save(item) {
+  save(item, isNew = false) {
     // Do save stuff here
-    this.edit = -1;
+    if (isNew) {
+      ELEMENT_DATA.push({ abbreviation: this.abbreviation.value, name: this.name.value, mode: this.sync.value });
+      this.table.renderRows();
+      this.clearFormControls();
+    }
+    // this.data = new  (ELEMENT_DATA);
+    this.edit = null;
   }
 
   delete(toDelete) {
     // Perform deletion
   }
 
-  editing(id): boolean {
-    return id === this.edit;
+  editing(item): boolean {
+    return item === this.edit;
   }
 
-  flagEdit(id) {
-    this.edit = id;
+  flagEdit(item) {
+    this.edit = item;
   }
 }
