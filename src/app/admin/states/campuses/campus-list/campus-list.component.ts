d0913f0b4bff3827e40d8e0c406f1c13af9a8ad1
Reuben Wilson <reubenmwilson@gmail.com>
2019-10-10 14:31:57 +1100

    enhance: Use new entity service for campus list

--- src/app/admin/states/campuses/campus-list/campus-list.component.ts
+++ src/app/admin/states/campuses/campus-list/campus-list.component.ts
@@ -3,35 +3,34 @@ import { currentUser, alertService } from 'src/app/ajs-upgraded-providers';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource, MatTable } from '@angular/material/table';
 import { FormControl } from '@angular/forms';
-
-let ELEMENT_DATA = [
-  { abbreviation: 'ABBR-01', name: 'Waterfront', mode: 'Timetable' },
-  { abbreviation: 'ABBR-01', name: 'Cloud', mode: 'Automatic' }
-];
+import { CampusService } from 'src/app/api/models/campus/campus.service';
+import { Campus } from 'src/app/api/models/campus/campus';
 
 @Component({
   selector: 'campus-list',
   templateUrl: 'campus-list.component.html',
   styleUrls: ['campus-list.component.scss']
 })
-export class CampusListComponent {
+export class CampusListComponent implements OnInit {
   @ViewChild(MatTable, { static: true }) table: MatTable<any>;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+
   // Form controls
   abbreviation = new FormControl('');
   name = new FormControl('');
   sync = new FormControl('');
+
   // Set up the table
   columns: string[] = ['abbreviation', 'name', 'mode', 'options'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  campuses: Campus[] = new Array<Campus>();
+  dataSource = new MatTableDataSource(this.campuses);
 
-  syncModes = ['Timetable', 'Automatic', 'None'];
-  edit: any;
-
-
+  syncModes = ['timetable', 'automatic', 'manual'];
+  selectedCampus: Campus;
 
   constructor(
-    @Inject(currentUser) private currentUser: any,
+    private campusService: CampusService,
+    // tslint:disable-next-line: no-shadowed-variable
     @Inject(alertService) private alertService: any
   ) { }
 
@@ -41,34 +40,51 @@ export class CampusListComponent {
     this.sync.setValue('');
   }
 
-  // ngOnInit() {
-  //   this.data.sort = this.sort;
-  // }
+  ngOnInit() {
+    this.campusService.query().subscribe((campuses) => {
+      this.campuses.push(...campuses);
+      this.table.renderRows();
+    });
+  }
 
   applyFilter(filterValue: string) {
     // this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  save(item, isNew = false) {
-    // Do save stuff here
-    if (isNew) {
-      ELEMENT_DATA.push({ abbreviation: this.abbreviation.value, name: this.name.value, mode: this.sync.value });
+  update(campus: Campus) {
+    this.campusService.update(campus).subscribe(
+      () => {
+        this.alertService.add('success', 'Campus edited', 2000);
+      },
+      error => {
+        this.alertService.add('danger', error, 2000);
+      });
+    this.selectedCampus = null;
+  }
+
+  delete(campus: Campus) {
+    this.campusService.delete(campus).subscribe();
+    this.selectedCampus = null;
+  }
+
+  saveNew() {
+    const newCampus = new Campus({ abbreviation: this.abbreviation.value, name: this.name.value, mode: this.sync.value });
+    this.campusService.create(newCampus).subscribe(result => {
+      this.campuses.push(result);
       this.table.renderRows();
       this.clearFormControls();
-    }
-    // this.data = new  (ELEMENT_DATA);
-    this.edit = null;
+    });
   }
 
-  delete(toDelete) {
-    // Perform deletion
+  cancelEdit() {
+    this.selectedCampus = null;
   }
 
-  editing(item): boolean {
-    return item === this.edit;
+  editing(item: Campus): boolean {
+    return item === this.selectedCampus;
   }
 
   flagEdit(item) {
-    this.edit = item;
+    this.selectedCampus = item;
   }
 }
