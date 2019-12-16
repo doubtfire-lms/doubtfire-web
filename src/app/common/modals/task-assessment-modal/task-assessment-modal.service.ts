c7c1ec286cb3c8a1be19d5b9d756151b222541ac
Maksym Slavnenko <mslavnenko@gmail.com>
2019-12-17 09:57:30 +1100

    FIX:  remove Ant Design import from task assessment modal.

--- src/app/common/modals/task-assessment-modal/task-assessment-modal.service.ts
+++ src/app/common/modals/task-assessment-modal/task-assessment-modal.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import {TaskAssessmentModalComponent} from './task-assessment-modal.component';
-import { TaskAssessmentResult } from '../../services/task-submission.service';
-import { TaskSubmissionHistoryComponent } from 'src/app/tasks/task-submission-history/task-submission-history.component';
-import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Injectable({
   providedIn: 'root'
