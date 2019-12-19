ffae95c4f1d2d29bb49120daf31a9a20035a9884
Maksym Slavnenko <mslavnenko@gmail.com>
2019-12-19 14:34:54 +1100

    QUALITY: separate method to check whether there are any submissions.

--- src/app/tasks/task-definition-editor/task-assessor/task-assessor.component.ts
+++ src/app/tasks/task-definition-editor/task-assessor/task-assessor.component.ts
@@ -24,48 +24,37 @@ export class TaskAssessorComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.submissions.getLatestSubmissionsTimestamps(this.task)
-    .subscribe(
-      (result) => {
-        if (result) {
-          this._hasAnySubmissions = true;
-          this.task.has_any_submissions = true;
-        }
-      },
-      (error) => {
-        if (error) {
-          this._hasAnySubmissions = false;
-          this.task.has_any_submissions = false;
-        }
-      }
-    );
+    this.hasAnySubmissions();
   }
 
   ngOnChanges() {
-    this.submissions.getLatestSubmissionsTimestamps(this.task)
-    .subscribe(
-      (result) => {
-        if (result) {
-          this._hasAnySubmissions = true;
-          this.task.has_any_submissions = true;
-        }
-      },
-      (error) => {
-        if (error) {
-          this._hasAnySubmissions = false;
-          this.task.has_any_submissions = false;
-        }
-      }
-    );
+    this.hasAnySubmissions();
   }
 
   testSubmission() {
-    // this.task.unit = () => this.unit;
     this.task.unit_id = this.unit.id;
     this.ts.presentTaskSubmissionModal(this.task, this.task.status, false, true);
   }
 
   testSubmissionHistory() {
     this.modalService.show(this.task);
+  }
+
+  hasAnySubmissions() {
+    this.submissions.getLatestSubmissionsTimestamps(this.task)
+    .subscribe(
+      (result) => {
+        if (result) {
+          this._hasAnySubmissions = true;
+          this.task.has_any_submissions = true;
+        }
+      },
+      (error) => {
+        if (error) {
+          this._hasAnySubmissions = false;
+          this.task.has_any_submissions = false;
+        }
+      }
+    );
   }
 }
