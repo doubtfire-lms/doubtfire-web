113a1da912f482abf5bd285f28e1c38df862a73b
reubsinit <reubenmwilson@gmail.com>
2020-02-07 10:55:42 +1100

    NEW: Barebones rendering of uit students

--- src/app/units/states/edit/directives/unit-students-editor/unit-students-editor.component.ts
+++ src/app/units/states/edit/directives/unit-students-editor/unit-students-editor.component.ts
@@ -1,6 +1,7 @@
-import { ViewChild, Component } from '@angular/core';
-import { MatTable } from '@angular/material/table';
+import { ViewChild, Component, Input, Inject } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { alertService } from 'src/app/ajs-upgraded-providers';
 
 @Component({
   selector: 'unit-students-editor',
@@ -10,14 +11,21 @@ import { MatSort } from '@angular/material/sort';
 export class UnitStudentsEditorComponent {
   @ViewChild(MatTable, { static: true }) table: MatTable<any>;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @Input() unit: any;
+
+  columns: string[] = ['username', 'firstName', 'lastName', 'email', 'tutorial', 'enrolled'];
+  dataSource: MatTableDataSource<any>;
 
 
   // Calls the parent's constructor, passing in an object
   // that maps all of the form controls that this form consists of.
   constructor(
+    @Inject(alertService) private alerts: any,
   ) {
   }
 
   ngOnInit() {
+    console.log(this.unit.students);
+    this.dataSource = new MatTableDataSource(this.unit.students);
   }
 }
