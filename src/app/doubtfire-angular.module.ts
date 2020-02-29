f93383e97d0a6c4fbda6e73307cf89bf0ee8ba6d
Jake Renzella <j@kerenzella.com>
2020-02-29 22:45:39 +1100

    FIX: Register service worker after 5s delay

--- src/app/doubtfire-angular.module.ts
+++ src/app/doubtfire-angular.module.ts
@@ -162,7 +162,7 @@ import { environment } from '../environments/environment';
     ReactiveFormsModule,
     PopoverModule.forRoot(),
     UIRouterUpgradeModule.forRoot({ states: doubtfireStates }),
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production, registrationStrategy: 'registerImmediately' }),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production, registrationStrategy: 'registerWithDelay:5000' }),
   ],
   // Services
   providers: [
