cd88b05b9863c00d95668fdc5d570586c184faf7
Jake Renzella <j@kerenzella.com>
2020-03-30 21:38:19 +1100

    (WIP): Progress on redesigning tcv

--- src/app/common/directives/drag-drop.directive.ts
+++ src/app/common/directives/drag-drop.directive.ts
@@ -6,31 +6,31 @@ import { Directive, Output, EventEmitter, HostBinding, HostListener } from '@ang
 
   @Output() onFileDropped = new EventEmitter<any>();
 
-  @HostBinding('style.background-color') private background = '#f5fcff';
-  @HostBinding('style.opacity') private opacity = '1';
+  // @HostBinding('style.background-color') private background = '#f5fcff';
+  // @HostBinding('style.opacity') private opacity = '1';
 
   // Dragover listener
   @HostListener('dragover', ['$event']) onDragOver(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.background = '#9ecbec';
-    this.opacity = '0.8';
+    // this.background = '#9ecbec';
+    // this.opacity = '0.8';
   }
 
   // Dragleave listener
   @HostListener('dragleave', ['$event']) public onDragLeave(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.background = '#f5fcff';
-    this.opacity = '1';
+    // this.background = '#f5fcff';
+    // this.opacity = '1';
   }
 
   // Drop listener
   @HostListener('drop', ['$event']) public ondrop(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.background = '#f5fcff';
-    this.opacity = '1';
+    // this.background = '#f5fcff';
+    // this.opacity = '1';
     let files = evt.dataTransfer.files;
     if (files.length > 0) {
       this.onFileDropped.emit(files);
