f1c67210faa2386732475472869bbded1f14ea0e
Jake Renzella <j@kerenzella.com>
2020-04-09 01:06:30 +1000

    NEW: Finalise new user-icon

--- src/app/common/user-icon/user-icon.component.ts
+++ src/app/common/user-icon/user-icon.component.ts
@@ -17,205 +17,120 @@ export class UserIconComponent implements AfterViewInit {
 
   @ViewChild('svg') svg;
 
+  lineHeight = 12;
+
   constructor(
     @Inject(currentUser) private currentUser: any,
   ) {
 
   }
 
-  // measureWidth = {
-  //   const context = document.createElement("canvas").getContext("2d");
-  //   return text => context.measureText(text).width;
-  // }
+  get backgroundUrl(): string {
+    let hash = this.email != null ? Md5.hashStr(this.email.trim().toLowerCase()) : Md5.hashStr('');
+    return `https://www.gravatar.com/avatar/${hash}.png?default=blank&size=${this.size}`;
+  }
 
-  measureWidth(text) {
+  get initials(): string {
+    let result = (this.user?.name != null) ? this.user.name.split(' ') : '  ';
+    return (result.length > 1) ? (`${result[0][0]}${result[1][0]}`).toUpperCase() : '  ';
+  }
+
+  get words(): string[] {
+    return [this.initials];
+  }
+
+  get targetWidth(): number {
+    return Math.sqrt(this.measureWidth(this.initials.trim()) * this.lineHeight);
+  }
+
+  get radius(): number {
+    return Math.min(this.size, this.size) / 2 - 4;
+  }
+
+  private generateLines(): any[] {
+    let line;
+    let lineWidth0 = Infinity;
+    const result = [];
+    for (let i = 0, n = this.words.length; i < n; ++i) {
+      let lineText1 = (line ? line.text + ' ' : '') + this.words[i];
+      let lineWidth1 = this.measureWidth(lineText1);
+      if ((lineWidth0 + lineWidth1) / 2 < this.targetWidth) {
+        line.width = lineWidth0 = lineWidth1;
+        line.text = lineText1;
+      } else {
+        lineWidth0 = this.measureWidth(this.words[i]);
+        line = { width: lineWidth0, text: this.words[i] };
+        result.push(line);
+      }
+    }
+    return result;
+  }
+
+  private generateUniqueId(): string {
+    return Math.random().toString(36).substring(2);
+  }
+
+  measureWidth(text: string): number {
     const context = document.createElement('canvas').getContext('2d');
     return context.measureText(text).width;
   }
 
+  private appendUserIcon(): void {
+
+  }
+
   ngAfterViewInit(): void {
-
-    const radius = Math.min(this.size, this.size) / 2 - 4;
-
-    let initials = (this.user?.name != null) ? this.user.name.split(' ') : '  ';
-    initials = (initials.length > 1) ? (`${initials[0][0]}${initials[1][0]}`).toUpperCase() : '  ';
-    let words = [initials];
-
-    const lineHeight = 12;
-
-    const targetWidth = Math.sqrt(this.measureWidth(initials.trim()) * lineHeight);
-
-    let line;
-    let lineWidth0 = Infinity;
-    const lines = [];
-    for (let i = 0, n = words.length; i < n; ++i) {
-      let lineText1 = (line ? line.text + ' ' : '') + words[i];
-      let lineWidth1 = this.measureWidth(lineText1);
-      if ((lineWidth0 + lineWidth1) / 2 < targetWidth) {
-        line.width = lineWidth0 = lineWidth1;
-        line.text = lineText1;
-      } else {
-        lineWidth0 = this.measureWidth(words[i]);
-        line = { width: lineWidth0, text: words[i] };
-        lines.push(line);
-      }
-    }
+    const lines = this.generateLines();
 
     let textRadius = 0;
     for (let i = 0, n = lines.length; i < n; ++i) {
-      const dy = (Math.abs(i - n / 2 + 0.5) + 0.5) * lineHeight;
+      const dy = (Math.abs(i - n / 2 + 0.5) + 0.5) * this.lineHeight;
       const dx = lines[i].width / 2;
       textRadius = Math.max(textRadius, Math.sqrt(dx ** 2 + dy ** 2));
     }
 
     const svg = d3.select(this.svg.nativeElement)
       .style('font', '10px sans-serif')
-      .style('max-width', '100%')
-      .style('height', 'auto')
+      .attr('width', this.size)
+      .attr('height', this.size)
       .attr('text-anchor', 'middle');
+
+    function appendCircle(selection, size) {
+      selection.append('circle')
+        .attr('cx', size / 2)
+        .attr('cy', size / 2)
+        .attr('r', this.radius);
+    }
+
+    const id = this.generateUniqueId();
+
+    let defs = svg.append('defs');
+    defs.append('clipPath')
+      .attr('id', `image-clip-${id}`)
+      .call(appendCircle, this.size);
 
     svg.append('circle')
       .attr('cx', this.size / 2)
       .attr('cy', this.size / 2)
-      .attr('fill', '#ccc')
-      .attr('r', radius);
+      .attr('r', this.radius)
+      .attr('fill', '#9e9e9e');
 
     svg.append('text')
-      .attr('transform', `translate(${this.size / 2},${this.size / 2}) scale(${radius / textRadius})`)
+      .attr('transform', `translate(${this.size / 2},${this.size / 2}) scale(${this.radius / textRadius})`)
       .selectAll('tspan')
       .data(lines)
       .enter().append('tspan')
       .attr('x', 0)
-      .attr('y', (d, i) => (i - 1 / 2 + 0.8) * lineHeight)
+      .attr('y', (d, i) => (i - 1 / 2 + 0.8) * this.lineHeight)
+      .attr('fill', 'white')
       .text(d => d.text);
 
-    // return svg.node();
-
+    svg.append('image')
+      .attr('xlink:href', this.backgroundUrl)
+      .attr('width', `${this.size}px`)
+      .attr('height', `${this.size}px`)
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('clip-path', `url(#image-clip-${id})`);
   }
-
 }
-
-//   get targetWidth() {
-//     return Math.sqrt(this.measureWidth * this.lineHeight);
-//   }
-
-//   // this is an offscreen canvase just to see how wide the text is.
-//   get measureWidth() {
-//     const context = document.createElement('canvas').getContext('2d');
-//     return context.measureText(this.initials.trim()).width;
-//   }
-
-//   // lines() {
-//   //   let line;
-//   //   let lineWidth0 = Infinity;
-//   //   const lines = [];
-//   //   for (let i = 0, n = this.initials.length; i < n; ++i) {
-//   //     let lineText1 = (line ? line.text + ' ' : "") + this.initials[i];
-//   //     let lineWidth1 = measureWidth(lineText1);
-//   //     if ((lineWidth0 + lineWidth1) / 2 < targetWidth) {
-//   //       line.width = lineWidth0 = lineWidth1;
-//   //       line.text = lineText1;
-//   //     } else {
-//   //       lineWidth0 = measureWidth(this.initials[i]);
-//   //       line = { width: lineWidth0, text: this.initials[i] };
-//   //       lines.push(line);
-//   //     }
-//   //   }
-//   //   return lines;
-//   // }
-
-//   calculateSVGCircle() {
-//     const width = this.size;
-//     const height = width;
-//     const radius = Math.min(width, height) / 2 - 4;
-
-//     const svg = d3.select(this.svg)
-//       .style('font', '10px sans-serif')
-//       .style('max-width', '100%')
-//       .style('height', 'auto')
-//       .attr('text-anchor', 'middle');
-
-//     svg.append('circle')
-//       .attr('cx', width / 2)
-//       .attr('cy', height / 2)
-//       .attr('fill', '#ccc')
-//       .attr('r', radius);
-
-//     svg.append('text')
-//       .attr('transform', `translate(${width / 2},${height / 2}) scale(${radius / textRadius})`)
-//       .selectAll('tspan')
-//       .data(lines)
-//       .enter().append('tspan')
-//       .attr('x', 0)
-//       .attr('y', (d, i) => (i - lines.length / 2 + 0.8) * lineHeight)
-//       .text(d => d.text);
-
-//     return svg.node();
-
-//   }
-
-//   get backgroundUrl(): string {
-//     let hash = this.email != null ? Md5.hashStr(this.email.trim().toLowerCase()) : Md5.hashStr('');
-//     return `https://www.gravatar.com/avatar/${hash}.png?default=blank&size=${this.size}`;
-//   }
-
-//   userBackgroundStyle(email: string): object {
-//     // Gravatar hash
-//     let hash = email != null ? Md5.hashStr(this.email.trim().toLowerCase()) : Md5.hashStr('');
-
-//     let backgroundUrl = `https://www.gravatar.com/avatar/${hash}.png?default=blank&size=${this.size}`;
-//     return {
-//       'background-image': `url('${backgroundUrl}')`
-//     };
-//   }
-
-//   get textRadius() {
-//     let radius = 0;
-
-//     const dy = (Math.abs(i - n / 2 + 0.5) + 0.5) * this.lineHeight;
-//     const dx = lines[i].width / 2;
-//     radius = Math.max(radius, Math.sqrt(dx ** 2 + dy ** 2));
-//     return radius;
-//   }
-
-//   get componentSize() {
-//     return {
-//       'width.px': this.size,
-//       'height.px': this.size,
-//       'min.width.px': this.size,
-//       'min.height.px': this.size
-//     };
-//   }
-
-//   get initials() {
-//     let initials = (this.user?.name != null) ? this.user.name.split(' ') : '  ';
-//     return (initials.length > 1) ? (`${initials[0][0]}${initials[1][0]}`).toUpperCase() : '  ';
-//   }
-// }
-
-
-// angular.module('doubtfire.common.user-icon', [])
-// .directive('userIcon', ->
-//   restrict: 'E'
-//   replace: true
-//   scope:
-//     user: '=?'
-//     size: '=?'
-//     email: '=?'
-//   templateUrl: 'common/user-icon/user-icon.tpl.html'
-//   controller: (this, $http, currentUser, md5) ->
-//     $scope.user ?= currentUser.profile
-//     $scope.size ?= 100
-//     $scope.email ?= $scope.user.email
-
-//     $scope.userBackgroundStyle = (email) ->
-//       # Gravatar hash
-//       hash = if (email) then md5.createHash(email.trim().toLowerCase()) else md5.createHash('')
-//       backgroundUrl = 'https://www.gravatar.com/avatar/#{hash}.png?default=blank&size=#{$scope.size}'
-//       'background-image: url('#{backgroundUrl}')'
-
-//     $scope.initials = (->
-//       initials = if ($scope.user && $scope.user.name) then $scope.user.name.split(' ') else '  '
-//       if initials.length > 1 then ('#{initials[0][0]}#{initials[1][0]}').toUpperCase() else '  '
-//     )()
-// )
