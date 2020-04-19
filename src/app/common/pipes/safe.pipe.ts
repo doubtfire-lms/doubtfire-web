f201a16acb3b3aed7b4b5133083506254d85e228
Jake Renzella <j@kerenzella.com>
2020-04-19 15:55:47 +1000

    FIX: Ensure safe works for pdf urls

--- src/app/common/pipes/safe.pipe.ts
+++ src/app/common/pipes/safe.pipe.ts
@@ -1,10 +1,10 @@
-import { Pipe, PipeTransform, SecurityContext } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer} from '@angular/platform-browser';
 
 @Pipe({ name: 'safe' })
 export class SafePipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
   transform(url: string) {
-    return this.sanitizer.sanitize(SecurityContext.URL, url);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
