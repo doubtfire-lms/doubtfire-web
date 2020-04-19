23d4e8061aeec6ecdfdb5e0dd78101ecdab7f990
Jake Renzella <j@kerenzella.com>
2020-04-19 15:38:43 +1000

    NEW: Add santiizer safe pipe

---
+++ src/app/common/pipes/safe.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform, SecurityContext } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
+  transform(url: string) {
+    return this.sanitizer.sanitize(SecurityContext.URL, url);
+  }
+}
