b15e2b460e2a3cbb91c3ef663b130282613f59d5
Jake Renzella <j@kerenzella.com>
2020-04-20 03:02:34 +1000

    FIX: Fix logic in task def pipe

--- src/app/common/filters/tasks-of-task-definition.pipe.ts
+++ src/app/common/filters/tasks-of-task-definition.pipe.ts
@@ -6,9 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TasksOfTaskDefinitionPipe implements PipeTransform {
 
   transform(tasks, taskDefinition): any[] {
-    if (!((taskDefinition) && (tasks))) {
+    if ((taskDefinition == null) || (tasks == null)) {
       return tasks;
     }
-    return tasks = tasks.filter({ task_definition_id: taskDefinition.id });
+    return tasks.filter( task => task.task_definition_id === taskDefinition.id );
   }
 }
