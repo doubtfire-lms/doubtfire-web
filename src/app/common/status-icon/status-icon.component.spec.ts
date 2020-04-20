e428b4885f7812160ca782ebea95f9cdbc7ee3fc
Jake Renzella <j@kerenzella.com>
2020-04-20 15:02:42 +1000

    NEW: Rewrite status-icon component

---
+++ src/app/common/status-icon/status-icon.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StatusIconComponent } from './status-icon.component';
+
+describe('StatusIconComponent', () => {
+  let component: StatusIconComponent;
+  let fixture: ComponentFixture<StatusIconComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StatusIconComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StatusIconComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
