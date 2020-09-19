b350a7e3b832af1bde6ee262082008604e0195f8
Jake Renzella <j@kerenzella.com>
2020-09-20 09:46:52 +1000

    QUALITY: Adhere to TSLint rules in activity-type

--- src/app/api/models/activity-type/activity-type.service.ts
+++ src/app/api/models/activity-type/activity-type.service.ts
@@ -1,15 +1,14 @@
 import { CacheableEntityService } from '../cacheable-entity.service';
 import { ActivityType } from './activity-type';
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ActivityTypeService extends CacheableEntityService<ActivityType> {
-
   protected readonly endpointFormat = 'activity_types/:id:';
   entityName = 'ActivityType';
 
   protected createInstanceFrom(json: any, other?: any): ActivityType {
-    let activityType = new ActivityType();
+    const activityType = new ActivityType();
     activityType.updateFromJson(json);
     return activityType;
   }
