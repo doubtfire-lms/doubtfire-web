7fb78eb96c8ba31e6cca849eaeb1fffb68cac4b7
Jake Renzella <j@kerenzella.com>
2020-09-19 20:42:53 +1000

    QUALITY: Various TSLint adherences

--- src/app/tasks/task-comments-viewer/task-comments-viewer.component.spec.ts
+++ src/app/tasks/task-comments-viewer/task-comments-viewer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TaskCommentsViewerComponent } from './task-comments-viewer.component';
 
@@ -6,12 +6,13 @@ describe('TaskCommentsViewerComponent', () => {
   let component: TaskCommentsViewerComponent;
   let fixture: ComponentFixture<TaskCommentsViewerComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TaskCommentsViewerComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [TaskCommentsViewerComponent],
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TaskCommentsViewerComponent);
