8eea372e71266c794d20f5393b1fe8d006cbf6e3
Andrew Cain <macite@gmail.com>
2021-02-15 09:45:56 +1100

    QUALITY: Remove unused code from task comment view service

--- src/app/common/services/task-comment.service.ts
+++ src/app/common/services/task-comment.service.ts
@@ -35,37 +35,19 @@ export class TaskCommentViewService {
     }
   }
 
-  replyToComment(originalCommentID) {
-    this.ts.currentReplyID.id = originalCommentID;
-  }
-
-  // reactToComment(originalComment) {
-  // }
-
-  isStaff(project: any) {
-    return project.unit().my_role !== 'Student';
-  }
-
-  canUserEdit(comment, project) {
-    return comment.author_is_me || this.isStaff(project);
-  }
-
-  isBubbleComment(comment) {
-    return this.ts.isBubbleComment(comment.type);
-  }
-
   deleteComment(task, comment) {
-    this.tc.delete(
-      { project_id: task.project().project_id, task_definition_id: task.task_definition_id, id: comment.id },
-      (success) => {
-        let comments = task.comments.filter((e) => e.id !== comment.id);
-        comments = this.ts.mapComments(comments);
-        task.comments = comments;
-        this.analytics.event('View Task Comments', 'Deleted existing comment');
-      },
-      (error) => {
-        this.alerts.add('danger', error.data.error, 2000);
-      }
-    );
+    console.log('to add code...');
+    // this.tc.delete(
+    //   { project_id: task.project().project_id, task_definition_id: task.task_definition_id, id: comment.id },
+    //   (success) => {
+    //     let comments = task.comments.filter((e) => e.id !== comment.id);
+    //     comments = this.ts.mapComments(comments);
+    //     task.comments = comments;
+    //     this.analytics.event('View Task Comments', 'Deleted existing comment');
+    //   },
+    //   (error) => {
+    //     this.alerts.add('danger', error.data.error, 2000);
+    //   }
+    // );
   }
 }
