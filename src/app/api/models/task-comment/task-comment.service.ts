18922acbb6183032e63237ef352673ae3fcc3465
Andrew Cain <macite@gmail.com>
2021-03-12 16:32:26 +1100

    ENHANCE: Update task comment service to support discussion comments

--- src/app/api/models/task-comment/task-comment.service.ts
+++ src/app/api/models/task-comment/task-comment.service.ts
@@ -1,12 +1,19 @@
 import { TaskComment } from 'src/app/api/models/doubtfire-model';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { CachedEntityService } from '../cached-entity.service';
+import { HttpOptions } from '../entity.service';
 
 @Injectable()
 export class TaskCommentService extends CachedEntityService<TaskComment> {
-  protected readonly endpointFormat = 'projects/:projectId:/task_def_id/:taskDefinitionId:/comments/:id:';
+  public readonly commentAdded$: EventEmitter<TaskComment> = new EventEmitter();
+
+  private readonly commentEndpointFormat = 'projects/:projectId:/task_def_id/:taskDefinitionId:/comments/:id:';
+  private readonly discussionEndpointFormat =
+    '/projects/:projectId:/task_def_id/:taskDefinitionId:/discussion_comments';
+
+  protected readonly endpointFormat = this.commentEndpointFormat;
   entityName = 'TaskComment';
 
   protected createInstanceFrom(json: any, other?: any): TaskComment {
@@ -21,7 +28,8 @@ export class TaskCommentService extends CachedEntityService<TaskComment> {
     task: any,
     data: string | File | Blob,
     commentType: string,
-    originalComment?: TaskComment
+    originalComment?: TaskComment,
+    prompts?: Blob[]
   ): Observable<TaskComment> {
     const pathId: {} = {
       projectId: task.project().project_id,
@@ -33,17 +41,27 @@ export class TaskCommentService extends CachedEntityService<TaskComment> {
       body.append('reply_to_id', originalComment?.id.toString());
     }
 
+    const opts: HttpOptions = { alternateEndpointFormat: this.commentEndpointFormat };
+
+    // Based on the comment type - add to the body and configure the end point
     if (commentType === 'text') {
       body.append('comment', data);
+    } else if (commentType === 'discussion') {
+      opts.alternateEndpointFormat = this.discussionEndpointFormat;
+      for (const prompt of prompts) {
+        body.append('attachments[]', prompt);
+      }
     } else {
       body.append('attachment', data);
     }
 
     this.cacheSource = task.commentCache;
-    return this.create(pathId, body, task).pipe(
+    const self = this;
+    return this.create(pathId, body, task, opts).pipe(
       tap((tc: TaskComment) => {
         task.comments.push(tc);
         task.refreshCommentData();
+        self.commentAdded$.emit(tc);
       })
     );
   }
