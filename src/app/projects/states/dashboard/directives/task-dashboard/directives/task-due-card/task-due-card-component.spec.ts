30546d064f19cbee33f603d497e0dca2f8810680
erickao5858 <eric.kao5858@gmail.com>
2021-04-12 21:08:31 +1000

    QUALITY: Format files

--- src/app/projects/states/dashboard/directives/task-dashboard/directives/task-due-card/task-due-card-component.spec.ts
+++ src/app/projects/states/dashboard/directives/task-dashboard/directives/task-due-card/task-due-card-component.spec.ts
@@ -1,26 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TaskDueCardComponent } from './task-due-card.component';
+
 describe('TaskDueCardComponent', () => {
-    let component: TaskDueCardComponent;
-    let fixture: ComponentFixture<TaskDueCardComponent>;
-  
-    beforeEach(
-      waitForAsync(() => {
-        TestBed.configureTestingModule({
-          declarations: [TaskDueCardComponent],
-        }).compileComponents();
-      })
-    );
-  
-    beforeEach(() => {
-      fixture = TestBed.createComponent(TaskDueCardComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    });
-  
-    it('should create the component', () => {
-      expect(component).toBeTruthy();
-    });
+  let component: TaskDueCardComponent;
+  let fixture: ComponentFixture<TaskDueCardComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [TaskDueCardComponent],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TaskDueCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
-  
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/projects/states/dashboard/directives/task-dashboard/directives/task-due-card/task-due-card.component.ts
+++ src/app/projects/states/dashboard/directives/task-dashboard/directives/task-due-card/task-due-card.component.ts
@@ -1,14 +1,11 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'task-due-card',
   templateUrl: 'task-due-card.component.html',
-  styleUrls: ['task-due-card.component.scss']
+  styleUrls: ['task-due-card.component.scss'],
 })
 export class TaskDueCardComponent {
   @Input() task: any;
-  constructor(
-  ) {
-    };
+  constructor() {}
 }
-
