c10b6b617d38824a3702b0aed17c8a13be12d35c
samhitha <samhitha makham>
2021-04-20 03:29:56 +1000

    NEW: Create initial filesfor migrationof task-ilo-alignment

---
+++ src/app/tasks/task-ilo-alignment/task-ilo-alignment-editor/task-ilo-alignment.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, Inject} from '@angular/core';
+@Component({
+    selector: 'task-ilo-alignment',
+    templateUrl: 'task-ilo-alignment.component.html',
+    styleUrls: ['task-ilo-description.component.scss']
+
+})
+export class TaskiloAlignmentComponent {
+    constructor (
+
+    ) {
+}
+}
