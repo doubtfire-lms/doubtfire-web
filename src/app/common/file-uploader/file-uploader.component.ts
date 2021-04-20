26e95842bb2de47bfa7b37ed7488a7bc063b4359
greeshmadantla7 <greeshmadantla@gmail.com>
2021-04-20 12:10:15 +1000

    NEW: Create initial files for migration of file-uploader

---
+++ src/app/common/file-uploader/file-uploader.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'file-uploader',
+  templateUrl: 'file-uploader.component.html',
+  styleUrls: ['file-uploader.component.scss'],
+})
+export class FileUploaderComponent {
+  constructor() {}
+}
