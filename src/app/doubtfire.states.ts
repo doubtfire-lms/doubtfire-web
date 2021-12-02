88b21071d441a9f8aa4ec1469b9524609c3f1aaa
ginge270 <63086783+ginge270@users.noreply.github.com>
2021-12-02 13:23:31 +1100

    refactor: adds new angular home component
    This adds the start of the new angular home component to replace the
    coffeescript files & bootstrap styling.
    
    Co-authored-by: Andrew Cain <macite@gmail.com>
    Co-authored-by: Nimduli Athukorala <NimduliAthukorala@users.noreply.github.com>

--- src/app/doubtfire.states.ts
+++ src/app/doubtfire.states.ts
@@ -1,6 +1,8 @@
 import { NgHybridStateDeclaration } from '@uirouter/angular-hybrid';
 import { Ng2ViewDeclaration } from '@uirouter/angular';
 import { InstitutionSettingsComponent } from './admin/institution-settings/institution-settings.component';
+import { NewHomeComponent } from './home/states/home/home.component';
+
 
 /*
  * Use this file to store any states that are sourced by angular components.
@@ -28,6 +30,27 @@ const institutionSettingsState: NgHybridStateDeclaration = {
 };
 
 /**
+ * Define the institution settings state - used to edit campus data.
+ */
+ const newHomeState: NgHybridStateDeclaration = {
+  name: 'new-home',
+  url: '/new-home',                     // You get here with this url
+  views: {                                                // These are the 2 views - the header and main from the body of DF
+    header: {                                             // Header is still angularjs
+      controller: 'BasicHeaderCtrl',                      // This is the angularjs controller
+      templateUrl: 'common/header/header.tpl.html'        // and the related template html
+    } as unknown as Ng2ViewDeclaration,                   // Need dodgy cast to get compiler to ignore type data
+    main: {                                               // Main body links to angular component
+      component: NewHomeComponent
+    },
+  },
+  data: {                                                 // Add data used by header
+    pageTitle: 'New Home Page',
+    roleWhitelist: ['Student', 'Tutor', 'Convenor', 'Admin']
+  }
+};
+
+/**
  * Export the list of states we have created in angular
  */
-export const doubtfireStates = [institutionSettingsState];
+export const doubtfireStates = [institutionSettingsState, newHomeState];
---
+++ src/app/home/states/home/home.component.html
@@ -0,0 +1,3 @@
+<div>
+  <p>Hello World</p>
+</div>
