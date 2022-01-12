e4d4d39171f40dd7e0fe1e34f9689fa8258730fb
Jake Renzella <j@kerenzella.com>
2022-01-12 22:16:22 +1100

    feat: finalise unit dropdown component

--- src/app/common/header/unit-dropdown/unit-dropdown.component.ts
+++ src/app/common/header/unit-dropdown/unit-dropdown.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { UIRouter } from '@uirouter/angular';
-import { dateService, projectService, unitService } from 'src/app/ajs-upgraded-providers';
-import { IsActiveUnitRole } from '../../pipes/is-active-unit-role.pipe';
+import { dateService } from 'src/app/ajs-upgraded-providers';
 
 @Component({
   selector: 'unit-dropdown',
@@ -9,46 +8,11 @@ import { IsActiveUnitRole } from '../../pipes/is-active-unit-role.pipe';
   styleUrls: ['./unit-dropdown.component.scss'],
 })
 export class UnitDropdownComponent implements OnInit {
+  @Input() unitRoles: any;
+  @Input() projects: any;
   @Input() unit: any;
-  unitRoles: any;
-  projects: any;
-  filteredRoles: any[];
   unitTitle: string;
-  constructor(
-    @Inject(dateService) private DateService: any,
-    @Inject(unitService) private UnitService: any,
-    @Inject(projectService) private ProjectService: any,
-    private isActiveUnitRole: IsActiveUnitRole,
-    private router: UIRouter,
-
-  ) {
-    this.UnitService.getUnitRoles((roles: any) => {
-      this.unitRoles = roles;
-
-      this.filteredRoles = this.isActiveUnitRole.transform(this.unitRoles);
-      this.filteredRoles = this.filteredRoles.filter((role) => this.isUniqueRole(role));
-    });
-
-    this.ProjectService.getProjects(false, (projects: any) => {
-      this.projects = projects;
-    });
-
-    this.router.transitionService
-      .onSuccess(
-        {
-          to: 'home',
-        },
-        (trans) => {
-          this.unit = null
-        }
-      )
-  }
-
-
-  isUniqueRole = (unit) => {
-    let units = this.unitRoles.filter((role: any) => role.unit_id === unit.unit_id);
-    return units.length == 1 || unit.role == 'Tutor';
-  };
+  constructor(@Inject(dateService) private DateService: any) {}
 
   ngOnInit(): void {}
 
