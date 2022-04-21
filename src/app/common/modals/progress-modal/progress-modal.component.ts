15f8d5390e8170f711c915cb1131ab0186bd4d2a
Anthony Papoutsis <anthony8121999@gmail.com>
2022-04-21 14:38:45 +1000

    change

--- src/app/common/modals/progress-modal/progress-modal.component.ts
+++ src/app/common/modals/progress-modal/progress-modal.component.ts
@@ -1,10 +1,26 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, Input, Inject, OnInit } from '@angular/core';
+import { taskService } from 'src/app/ajs-upgraded-providers';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'progress-modal',
   templateUrl: './progress-modal.component.html',
   styleUrls: ['./progress-modal.component.scss'],
 })
-export class ProgressModalComponent {
-  constructor() {}
+export class ProgressModalComponent implements OnInit {
+  @Input() title: any;
+  @Input() message: any;
+
+  constructor(
+    public dialogRef: MatDialogRef<ProgressModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(taskService) private bop: any
+  ) {}
+
+  ngOnInit() {
+    this.title = this.bop;
+    this.message = this.data;
+    console.log(this.title);
+    console.log(this.data);
+  }
 }
