e2fe103fda5434ef5a9e1dfe69888ad282e25fb5
Anthony Papoutsis <anthony8121999@gmail.com>
2022-04-21 11:58:27 +1000

    New: Create initial files for migration

---
+++ src/app/common/modals/progress-modal/progress-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'progress-modal',
+  templateUrl: './progress-modal.component.html',
+  styleUrls: ['./progress-modal.component.html'],
+})
+export class ProgressModalComponent {
+  constructor() {}
+}
