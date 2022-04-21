0a4ac6a784dfddaf3fa71bc84cd03a90c5cfe098
Anthony Papoutsis <anthony8121999@gmail.com>
2022-04-21 12:33:15 +1000

    New: added service to be able to view component

--- src/app/common/modals/progress-modal/progress-modal.component.ts
+++ src/app/common/modals/progress-modal/progress-modal.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Inject } from '@angular/core';
 @Component({
   selector: 'progress-modal',
   templateUrl: './progress-modal.component.html',
-  styleUrls: ['./progress-modal.component.html'],
+  styleUrls: ['./progress-modal.component.scss'],
 })
 export class ProgressModalComponent {
   constructor() {}
