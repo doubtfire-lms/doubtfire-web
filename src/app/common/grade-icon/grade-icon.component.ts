5cea7b6477c3ff10397da14722a6b4b91f317f16
Perry Rose <p@erryro.se>
2022-04-25 09:36:55 +1000

    new: create inital files for grade-icon migration

---
+++ src/app/common/grade-icon/grade-icon.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'grade-icon',
+  templateUrl: 'grade-icon.component.html',
+  styleUrls: ['grade-icon.component.scss'],
+})
+export class GradeIconComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+}
