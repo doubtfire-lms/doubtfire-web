ded3377b0aeb9168a7817df43ac3e00612106a22
Perry Rose <p@erryro.se>
2022-04-25 12:09:20 +1000

    new: create initial files for migration of grade-task-modal

---
+++ src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'grade-task-modal',
+  templateUrl: './grade-task-modal.component.html',
+  styleUrls: ['./grade-task-modal.component.scss']
+})
+export class GradeTaskModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
