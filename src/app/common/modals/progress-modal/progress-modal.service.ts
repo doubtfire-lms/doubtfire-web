c0b4cd6f595d317fea9aac066a97c52b9f017ae6
Anthony Papoutsis <anthony8121999@gmail.com>
2022-05-02 16:34:04 +1000

    migrate: functionality

--- src/app/common/modals/progress-modal/progress-modal.service.ts
+++ src/app/common/modals/progress-modal/progress-modal.service.ts
@@ -8,8 +8,15 @@ import { ProgressModalComponent } from './progress-modal.component';
 export class ProgressModalService {
   constructor(public dialog: MatDialog) {}
 
-  public show() {
+  public show(title: any, message: any) {
     let dialogRef: MatDialogRef<ProgressModalComponent, any>;
-    dialogRef = this.dialog.open(ProgressModalComponent, {});
+    dialogRef = this.dialog.open(ProgressModalComponent, {
+      data: {
+        title,
+        message,
+      },
+    });
+    dialogRef.afterOpened().subscribe((result: any) => {});
+    dialogRef.afterClosed().subscribe((result: any) => {});
   }
 }
