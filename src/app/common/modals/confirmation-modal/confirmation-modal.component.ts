ccd866beeab1939d51f415d8768bc7599d2a7072
Anthony Papoutsis <anthony8121999@gmail.com>
2022-05-05 12:06:39 +1000

    create: new files for component

---
+++ src/app/common/modals/confirmation-modal/confirmation-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'confirmation-modal',
+  templateUrl: 'confirmation-modal.component.html',
+  styleUrls: ['./confirmation-modal.component.scss'],
+})
+export class ComponentModalComponent {
+  constructor() {}
+}
