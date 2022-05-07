7c2400058ff9984e604ed84eed1fb86ba0a955b2
Perry Rose <p@erryro.se>
2022-05-07 16:18:53 +1000

    migrate: complete migration for rating quality-point tasks

--- src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
+++ src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { gradeService } from 'src/app/ajs-upgraded-providers';
 
 @Component({
   selector: 'grade-task-modal',
   templateUrl: './grade-task-modal.component.html',
-  styleUrls: ['./grade-task-modal.component.scss']
+  styleUrls: ['./grade-task-modal.component.scss'],
 })
 export class GradeTaskModalComponent implements OnInit {
+  @Input() task: any;
+  @Input() data: any;
 
-  constructor() { }
+  totalRating;
+  newRating = 1;
+  gradeValues;
+  grades;
+
+  constructor(
+    public dialogRef: MatDialogRef<GradeTaskModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public dialogData: any,
+    @Inject(gradeService) private gradeService: any
+  ) { }
 
   ngOnInit(): void {
+    this.task = this.dialogData.task;
+
+    this.data = {
+      desiredGrade: this.task.grade,
+      rating: this.task.quality_pts || 1,
+      overStar: 0,
+      confRating: 0
+    };
+
+    this.totalRating = this.task.definition.max_quality_pts || 5;
+    this.gradeValues = this.gradeService.allGradeValues;
+    this.grades = this.gradeService.grades;
+  }
+
+  dismiss() {
+    this.dialogRef.close();
+  }
+
+  close() {
+    // Pass values back to service
+    this.dialogRef.close({
+      qualityPts: this.newRating,
+      selectedGrade: this.data.desiredGrade
+    });
+  }
+
+  updateRating(value: number) {
+    this.newRating = value;
   }
 
 }
