5f921b0805555c9ab9437ec5c1fcdad8317fe890
Perry Rose <p@erryro.se>
2022-05-12 19:33:52 +1000

    refactor: add ability to grade tasks, tidy up component

--- src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
+++ src/app/tasks/modals/grade-task-modal/grade-task-modal.component.ts
@@ -8,13 +8,16 @@ import { gradeService } from 'src/app/ajs-upgraded-providers';
   styleUrls: ['./grade-task-modal.component.scss'],
 })
 export class GradeTaskModalComponent implements OnInit {
-  @Input() task: any;
-  @Input() data: any;
+  task: any;
+  gradeValues: [number];
+  grades: [string];
 
-  totalRating;
-  newRating = 1;
-  gradeValues;
-  grades;
+  // Task Rating
+  totalRating: number;
+  rating: number;
+
+  // Grade Select
+  selectedGrade: number;
 
   constructor(
     public dialogRef: MatDialogRef<GradeTaskModalComponent>,
@@ -24,14 +27,7 @@ export class GradeTaskModalComponent implements OnInit {
 
   ngOnInit(): void {
     this.task = this.dialogData.task;
-
-    this.data = {
-      desiredGrade: this.task.grade,
-      rating: this.task.quality_pts || 1,
-      overStar: 0,
-      confRating: 0
-    };
-
+    this.rating = this.task.quality_pts || 1;
     this.totalRating = this.task.definition.max_quality_pts || 5;
     this.gradeValues = this.gradeService.allGradeValues;
     this.grades = this.gradeService.grades;
@@ -44,13 +40,21 @@ export class GradeTaskModalComponent implements OnInit {
   close() {
     // Pass values back to service
     this.dialogRef.close({
-      qualityPts: this.newRating,
-      selectedGrade: this.data.desiredGrade
+      qualityPts: this.rating,
+      selectedGrade: this.selectedGrade
     });
   }
 
   updateRating(value: number) {
-    this.newRating = value;
+    if (value <= this.totalRating) {
+      this.rating = value;
+    }
+  }
+
+  updateGrade(grade: number) {
+    if (this.gradeValues.includes(Number(grade))) {
+      this.selectedGrade = grade;
+    }
   }
 
 }
