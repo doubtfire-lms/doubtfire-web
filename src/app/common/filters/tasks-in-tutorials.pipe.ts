7c996f110054b7d0e5959eff96cdd90faf289725
Andrew Cain <macite@gmail.com>
2022-05-13 16:47:51 +1000

    feat: filter tasks in inbox based on task definition stream
    Ensure that tutors only see tasks associated with their tutorial
    when there are multiple streams.
    
    Co-authored-by: Ray Guo <RayGuo-ergou@users.noreply.github.com>

--- src/app/common/filters/tasks-in-tutorials.pipe.ts
+++ src/app/common/filters/tasks-in-tutorials.pipe.ts
@@ -4,23 +4,41 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'tasksInTutorials',
 })
 export class TasksInTutorialsPipe implements PipeTransform {
-  transform(tasks, tutorialIds: number[]): any[] {
+  transform(tasks, tutorialIds: number[], forceStream: boolean): any[] {
+    // Return nothing if there are no tasks
     if (!tasks) {
       return tasks;
     }
+
+    // Return nothing if there is no tutorialIds
     if (tutorialIds.length === 0) {
       return [];
     }
 
+    // Filter the tasks to only those where the tutorial for the task is in the list of tutorial ids
     const result = tasks?.filter((task) => {
-      if (task?.isGroupTask()) {
-        if (task.group() != null) {
-          const tute_id = task.group().tutorial_id;
-          return tutorialIds.includes(tute_id);
-        }
+      // Get the stream for the task... this may be nil or undefined if there are no streams in the unit
+      const stream = task.unit().tutorialStreamForAbbr(task.definition.tutorial_stream);
+
+      const useStream = forceStream && stream;
+
+      // If there is no stream, and the task is a group task, and there is a group for the task
+      if (!useStream && task?.isGroupTask() && task.group()) {
+        // use the group's tutorial
+        const tute_id = task.group().tutorial_id;
+        return tutorialIds.includes(tute_id); // check group tutorial id is in the list of ids to include
       } else {
-        const enrolments = task?.project().tutorial_enrolments;
-        return enrolments?.filter((enrolment) => tutorialIds.includes(enrolment.tutorial_id)).length > 0;
+        // either we have a stream, or its not a group task, or the student isn't in a group
+
+        if (forceStream) {
+          // Get the tutorial for the stream... this will return the only tutorial if there is no stream
+          const tutorial = task?.project().tutorialForStream(stream);
+          // Check it is in the list... enrolment may be null if the student is not enrolled in a tutorial
+          return tutorialIds.includes(tutorial?.id);
+        } else {
+          const enrolments = task?.project().tutorial_enrolments;
+          return enrolments?.filter((enrolment) => tutorialIds.includes(enrolment.tutorial_id)).length;
+        }
       }
     });
     return result;
