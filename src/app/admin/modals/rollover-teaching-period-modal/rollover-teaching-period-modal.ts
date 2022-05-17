7b1a4cdb327bce38aa6a85177097059855eecee5
Anthony Papoutsis <anthony8121999@gmail.com>
2022-05-17 10:56:44 +1000

    NEW: create the initial files and added template

---
+++ src/app/admin/modals/rollover-teaching-period-modal/rollover-teaching-period-modal.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'rollover-teaching-period-modal',
+  templateUrl: 'rollover-teaching-period-modal.html',
+  styleUrls: ['rollover-teaching-period-modal.scss'],
+})
+export class RolloverTeachingPeriodModalComponent {
+  constructor() {}
+}
