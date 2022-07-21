041f13d767ecee1f82597d2a1a69c025d516c7b1
Jake Renzella <j@kerenzella.com>
2022-07-22 01:22:43 +1000

    refactor: more specific type for systemRole in user.ts

--- src/app/api/models/user/user.ts
+++ src/app/api/models/user/user.ts
@@ -11,7 +11,7 @@ export class User extends Entity {
   email: string;
   username: string;
   nickname: string;
-  systemRole: string;
+  systemRole: 'Admin' | 'Convenor' | 'Tutor' | 'Student';
   receiveTaskNotifications: boolean;
   receivePortfolioNotifications: boolean;
   receiveFeedbackNotifications: boolean;
