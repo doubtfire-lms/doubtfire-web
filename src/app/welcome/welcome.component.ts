a80d9697a976b2689d5d78d1e14ac092c0fcd3a9
jakerenzella <j@kerenzella.com>
2022-07-24 19:54:35 +1000

    refactor: remove duplicated logic from welcome

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DoubtfireConstants } from 'src/app/config/constants/doubtfire-constants';
-import { UserService } from 'src/app/api/services/user.service';
-import { StateService } from '@uirouter/core';
-import { AuthenticationService } from '../api/services/authentication.service';
-import { GlobalStateService } from '../projects/states/index/global-state.service';
 
 @Component({
   selector: 'f-welcome',
@@ -11,47 +7,8 @@ import { GlobalStateService } from '../projects/states/index/global-state.servic
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
-  constructor(
-    private constants: DoubtfireConstants,
-    private userService: UserService,
-    private state: StateService,
-    private authService: AuthenticationService,
-    private globalState: GlobalStateService
-  ) {}
+  constructor(private constants: DoubtfireConstants) {}
+  ngOnInit(): void {}
 
   public externalName = this.constants.ExternalName;
-  public user = this.userService.currentUser;
-  public formPronouns = { pronouns: '' };
-  public get customPronouns(): boolean {
-    return this.formPronouns.pronouns === '__customPronouns';
-  }
-
-  ngOnInit(): void {
-    this.globalState.hideHeader();
-    if (this.userService.isAnonymousUser()) {
-      this.state.go('sign_in');
-    }
-
-    this.user.optInToResearch = false;
-    this.user.receiveFeedbackNotifications = true;
-    this.user.receivePortfolioNotifications = true;
-    this.user.receiveTaskNotifications = true;
-  }
-
-  public signOut(): void {
-    this.authService.signOut();
-  }
-
-  public submit(): void {
-    this.user.hasRunFirstTimeSetup = true;
-    this.user.pronouns = this.customPronouns ? this.user.pronouns : this.formPronouns.pronouns;
-
-    this.userService.update(this.user).subscribe({
-      next: (response) => {
-        this.userService.save(response);
-        this.state.go('home');
-      },
-      error: (error) => console.log(error),
-    });
-  }
 }
