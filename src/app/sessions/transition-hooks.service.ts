902e891cf597846a6d2327add770b558d4e9bc32
jakerenzella <j@kerenzella.com>
2022-07-24 20:09:43 +1000

    refactor: rewrite transition hooks service

--- src/app/sessions/transition-hooks.service.ts
+++ src/app/sessions/transition-hooks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TransitionService } from '@uirouter/angular';
-import { AppInjector } from '../app-injector';
+import { UserService } from '../api/services/user.service';
 import { DoubtfireAngularModule } from '../doubtfire-angular.module';
 import { GlobalStateService } from '../projects/states/index/global-state.service';
 
@@ -8,23 +8,27 @@ import { GlobalStateService } from '../projects/states/index/global-state.servic
   providedIn: DoubtfireAngularModule,
 })
 export class TransitionHooksService {
-  constructor() {
-    const transitions = AppInjector.get(TransitionService);
-    const globalState = AppInjector.get(GlobalStateService);
-    transitions.onBefore({}, (transition) => {
-      if (transition.to().name === 'sign_in') {
-        console.log('sign_in');
-        globalState.hideHeader();
-      }
-
-      if (transition.to().name === 'welcome') {
-        console.log('welcome');
-        globalState.hideHeader();
-      }
-
-      if (transition.to().name === 'home') {
-        console.log('home');
-        globalState.showHeader();
+  constructor(
+    private userService: UserService,
+    private transitions: TransitionService,
+    private globalState: GlobalStateService
+  ) {
+    this.transitions.onBefore({}, (transition) => {
+      switch (transition.to().name) {
+        case 'sign_in':
+          this.globalState.hideHeader();
+          break;
+        case 'welcome':
+          if (this.userService.currentUser.hasRunFirstTimeSetup || this.userService.isAnonymousUser()) {
+            return false;
+          }
+          this.globalState.hideHeader();
+          break;
+        case 'home':
+          this.globalState.showHeader();
+          break;
+        default:
+          break;
       }
     });
   }
