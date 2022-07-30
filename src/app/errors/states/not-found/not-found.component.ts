59f7d1660f11fe6886213652904b51769dc3e007
Nelson LAI <laisiotou1997@gmail.com>
2022-07-30 15:23:00 +1000

    NEW: Create initial files for migration of not-found

---
+++ src/app/errors/states/not-found/not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'task-description-card',
+  templateUrl: 'task-description-card.component.html',
+  styleUrls: ['task-description-card.component.scss'],
+})
+export class TaskDescriptionCardComponent {
+  constructor() {}
+}
