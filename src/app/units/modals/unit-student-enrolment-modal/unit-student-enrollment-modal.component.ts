427ec6e20d597307a7643bd3e70c922b088d2bde
Anthony Papoutsis <anthony8121999@gmail.com>
2022-08-19 19:00:45 +1000

    build: create initial files for migration of unit-student-enrollment-modal

---
+++ src/app/units/modals/unit-student-enrolment-modal/unit-student-enrollment-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'unit-student-enrollment-modal',
+  templateUrl: 'unit-student-enrollment-modal.component.html',
+  styleUrls: ['unit-student-enrollment-modal.component.scss'],
+})
+export class UnitStudentEnrollmentModalComponent {
+  constructor() {}
+}
