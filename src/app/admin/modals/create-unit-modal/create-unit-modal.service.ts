7b368add1d47ac4fd476f739af3e20a85d6ebb26
Anthony Papoutsis <anthony8121999@gmail.com>
2022-09-04 18:56:20 +1000

    build: create all files

---
+++ src/app/admin/modals/create-unit-modal/create-unit-modal.service.ts
@@ -0,0 +1,10 @@
+import { Component, Inject } from '@angular/core';
+
+@Component({
+  selector: 'create-unit-modal',
+  templateUrl: 'create-unit-modal.component.html',
+  styleUrls: ['create-unit-modal.component.scss'],
+})
+export class CreateUnitModalComponent {
+  constructor() {}
+}
