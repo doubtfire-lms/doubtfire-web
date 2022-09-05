e91fb7a69e42a5bbe25cea1858751865590a63a1
Anthony Papoutsis <anthony8121999@gmail.com>
2022-09-05 15:44:04 +1000

    build: initial files

---
+++ src/app/projects/states/portfolio/directives/portfolio-grade-select-step/portfolio-grade-select-step.component.ts
@@ -0,0 +1,10 @@
+import { Component, Inject } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-grade-select-step',
+  templateUrl: 'portfolio-grade-select-step.component.html',
+  styleUrls: ['portfolio-grade-select-step.component.scss'],
+})
+export class TaskDescriptionCardComponent {
+  constructor() {}
+}
