34ad625903c0689667867205ef1511359f49d7ac
Anthony Papoutsis <anthony8121999@gmail.com>
2022-09-06 12:45:10 +1000

    build: initial files

---
+++ src/app/projects/states/portfolio/directives/portfolio-learning-summary-report-step/portfolio-learning-summary-report-step.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-learning-summary-report-step',
+  templateUrl: 'portfolio-learning-summary-report-step.component.html',
+  styleUrls: ['portfolio-learning-summary-report-step.component.scss'],
+})
+export class TaskDescriptionCardComponent {
+  constructor() {}
+  ngOnInit() {}
+}
