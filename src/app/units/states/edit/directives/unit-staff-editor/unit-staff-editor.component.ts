2b0445fc8cd1f4926a4764fd9f0445b84dcf1f3b
Vriction <aldyputrashavira@gmail.com>
2022-09-08 19:35:21 +1000

    NEW: Create initial files for migration of unit-staff-editor

---
+++ src/app/units/states/edit/directives/unit-staff-editor/unit-staff-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component ({
+    selector: 'unit-staff-editor',
+    templateUrl: 'unit-staff-editor.component.html'
+})
+
+export class UnitStaffEditor {
+    contructor(){}
+}
