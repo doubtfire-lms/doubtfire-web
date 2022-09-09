cdeb7c624330054fb5ed1594288eaad7c8c2d87f
jakerenzella <j@kerenzella.com>
2022-09-10 02:04:00 +1000

    feat: add new user settings dialog

---
+++ src/app/common/modals/edit-profile-dialog/edit-profile-dialog.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { User } from 'src/app/api/models/doubtfire-model';
+import { EditProfileFormComponent } from '../../edit-profile-form/edit-profile-form.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EditProfileDialogService {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(user: User): void {
+    this.dialog.open(EditProfileFormComponent, {
+      data: { user },
+    });
+  }
+}
