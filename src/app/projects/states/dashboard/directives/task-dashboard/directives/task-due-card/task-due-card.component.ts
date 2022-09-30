c43ea3e27abdf253596bfedd5112f5ef7a7b929d
jakerenzella <j@kerenzella.com>
2022-10-01 00:24:32 +1000

    feat: add new task due card content

--- src/app/projects/states/dashboard/directives/task-dashboard/directives/task-due-card/task-due-card.component.ts
+++ src/app/projects/states/dashboard/directives/task-dashboard/directives/task-due-card/task-due-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from 'src/app/api/models/task';
 
 @Component({
   selector: 'f-task-due-card',
   templateUrl: './task-due-card.component.html',
-  styleUrls: ['./task-due-card.component.scss']
+  styleUrls: ['./task-due-card.component.scss'],
 })
 export class TaskDueCardComponent implements OnInit {
+  @Input() task: Task;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
