735c43b5c2df7e8f367310ad92ef716fd5140375
jakerenzella <j@kerenzella.com>
2022-10-05 20:30:46 +1100

    feat: add footer content

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from 'src/app/api/models/task';
+import { SelectedTaskService } from 'src/app/projects/states/dashboard/selected-task.service';
 
 @Component({
   selector: 'f-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor(private selectedTaskService: SelectedTaskService) {}
 
-  constructor() { }
+  selectedTask$: Observable<Task>;
 
   ngOnInit(): void {
-  }
+    // watch for changes to the selected task
+    this.selectedTask$ = this.selectedTaskService.selectedTask$;
 
+    // this.selectedTask$.subscribe(this.selectedTaskService.selectedTask$);
+    this.selectedTask$.subscribe((task) => {
+      console.log(task.project.student);
+    });
+  }
 }
