583ed0a667a1bdcd32c0c9419b86036e2bfe5817
lst97 <laisiotou1997@gmail.com>
2022-12-03 15:52:56 +1100

    test: create new state in angular

--- src/app/doubtfire.states.ts
+++ src/app/doubtfire.states.ts
@@ -2,7 +2,7 @@ import { NgHybridStateDeclaration } from '@uirouter/angular-hybrid';
 import { Ng2ViewDeclaration } from '@uirouter/angular';
 import { InstitutionSettingsComponent } from './admin/institution-settings/institution-settings.component';
 import { HomeComponent } from './home/states/home/home.component';
-
+import { TimeoutComponent } from './errors/states/timeout/timeout.component';
 
 /*
  * Use this file to store any states that are sourced by angular components.
@@ -50,7 +50,29 @@ const institutionSettingsState: NgHybridStateDeclaration = {
   }
 };
 
+const TimeoutState: NgHybridStateDeclaration = {
+  name: 'timeout',
+  url: '/timeout?dest&params', // You get here with this url
+  views: {
+    // These are the 2 views - the header and main from the body of DF
+    header: {
+      // Header is still angularjs
+      controller: 'BasicHeaderCtrl', // This is the angularjs controller
+      templateUrl: 'common/header/header.tpl.html', // and the related template html
+    } as unknown as Ng2ViewDeclaration, // Need dodgy cast to get compiler to ignore type data
+    main: {
+      // Main body links to angular component
+      component: TimeoutComponent,
+    },
+  },
+  data: {
+    // Add data used by header
+    pageTitle: 'Timeout',
+    roleWhitelist: ['Student', 'Tutor', 'Convenor', 'Admin'],
+  },
+};
+
 /**
  * Export the list of states we have created in angular
  */
-export const doubtfireStates = [institutionSettingsState, HomeState];
+export const doubtfireStates = [institutionSettingsState, HomeState, TimeoutState];
