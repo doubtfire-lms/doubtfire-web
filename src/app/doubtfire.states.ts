def036e7e4c03d2d5de011ac620c14d34e036873
lst97 <laisiotou1997@gmail.com>
2022-12-03 20:27:22 +1100

    test: create new angular state

--- src/app/doubtfire.states.ts
+++ src/app/doubtfire.states.ts
@@ -1,3 +1,4 @@
+import { UnauthorisedComponent } from './errors/states/unauthorised/unauthorised.component';
 import { NgHybridStateDeclaration } from '@uirouter/angular-hybrid';
 import { Ng2ViewDeclaration } from '@uirouter/angular';
 import { InstitutionSettingsComponent } from './admin/institution-settings/institution-settings.component';
@@ -50,7 +51,29 @@ const institutionSettingsState: NgHybridStateDeclaration = {
   }
 };
 
+const UnauthorisedState: NgHybridStateDeclaration = {
+  name: 'unauthorised',
+  url: '/unauthorised', // You get here with this url
+  views: {
+    // These are the 2 views - the header and main from the body of DF
+    header: {
+      // Header is still angularjs
+      controller: 'BasicHeaderCtrl', // This is the angularjs controller
+      templateUrl: 'common/header/header.tpl.html', // and the related template html
+    } as unknown as Ng2ViewDeclaration, // Need dodgy cast to get compiler to ignore type data
+    main: {
+      // Main body links to angular component
+      component: UnauthorisedComponent,
+    },
+  },
+  data: {
+    // Add data used by header
+    pageTitle: 'Unauthorised',
+    roleWhitelist: ['Student', 'Tutor', 'Convenor', 'Admin'],
+  },
+};
+
 /**
  * Export the list of states we have created in angular
  */
-export const doubtfireStates = [institutionSettingsState, HomeState];
+export const doubtfireStates = [institutionSettingsState, HomeState, UnauthorisedState];
