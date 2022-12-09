c86ea98a93e47b34072b66a4c22785d9fc7afb01
Chanputhi <Chanputhi9899@gmail.com>
2022-12-09 15:46:57 +1100

    build: create initial files for alert-list migration

---
+++ src/app/common/alert-list/alert-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "alert-list",
+    templateUrl: "./alert-list.component.html",
+    styleUrls: ["./alert-list.component.scss"]
+})
+export class AlertListComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
