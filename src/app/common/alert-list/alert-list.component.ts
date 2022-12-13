f399e1ece12d32a713e18749bed597a47dc53645
Chanputhi <Chanputhi9899@gmail.com>
2022-12-13 15:04:26 +1100

    Migrate: Make some change for alert-list component

--- src/app/common/alert-list/alert-list.component.ts
+++ src/app/common/alert-list/alert-list.component.ts
@@ -1,15 +1,4 @@
-// alert-list.coffee Angular code (old)
-// angular.module("doubtfire.common.alert-list", [])
-// .directive('alertList', ->
-//   restrict: 'E'
-//   templateUrl: 'common/alert-list/alert-list.tpl.html'
-//   replace: true
 
-//   controller: ($scope) ->
-// )
-
-
-// Convert to alert-list.component.ts Angular code (new)
 import { Component, Inject } from "@angular/core";
 import { alertService } from "src/app/ajs-upgraded-providers";
 
