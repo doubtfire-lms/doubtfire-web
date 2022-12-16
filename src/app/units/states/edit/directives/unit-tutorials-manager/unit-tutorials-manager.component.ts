2dbac37166af3c4594988b251d83ece49bd75529
lst97 <laisiotou1997@gmail.com>
2022-12-16 13:17:46 +1100

    refactor: alert in unit tutorials manager

--- src/app/units/states/edit/directives/unit-tutorials-manager/unit-tutorials-manager.component.ts
+++ src/app/units/states/edit/directives/unit-tutorials-manager/unit-tutorials-manager.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, Input, SimpleChanges } from '@angular/core';
-import { alertService } from 'src/app/ajs-upgraded-providers';
+import { Component, Input, SimpleChanges, OnInit } from '@angular/core';
+import { AlertService } from 'src/app/common/services/alert.service';
 import { ActivityType, ActivityTypeService, TutorialStream, Unit } from 'src/app/api/models/doubtfire-model';
 
 @Component({
@@ -7,16 +7,13 @@ import { ActivityType, ActivityTypeService, TutorialStream, Unit } from 'src/app
   templateUrl: 'unit-tutorials-manager.component.html',
   styleUrls: ['unit-tutorials-manager.component.scss'],
 })
-export class UnitTutorialsManagerComponent {
+export class UnitTutorialsManagerComponent implements OnInit {
   @Input() unit: Unit;
 
   activityTypes: ActivityType[] = new Array<ActivityType>();
   tutorialsByStream: any[] = new Array<any>();
 
-  constructor(
-    private activityTypeService: ActivityTypeService,
-    @Inject(alertService) private alertService: any
-  ) {}
+  constructor(private activityTypeService: ActivityTypeService, private alertService: AlertService) {}
 
   ngOnInit() {
     // Get the activity types
@@ -28,11 +25,11 @@ export class UnitTutorialsManagerComponent {
   onClickNewActivity(activity: ActivityType) {
     this.unit.nextStream(activity.abbreviation).subscribe({
       next: (value: TutorialStream) => {
-        this.alertService.add("success", `Added tutorial stream ${value.abbreviation}`, 2000);
+        this.alertService.success(`Added tutorial stream ${value.abbreviation}`);
       },
       error: (message) => {
-        this.alertService.add("danger", `Error creating tutorial stream: ${message}`, 8000);
-      }
+        this.alertService.danger(`Error creating tutorial stream: ${message}`);
+      },
     });
   }
 }
