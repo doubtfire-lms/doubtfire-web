eb801b4097b9a51b1a6a832385a88dee4cd94704
Chanputhi <Chanputhi9899@gmail.com>
2022-12-18 01:25:43 +1100

    test: add unit test for alert-list component

---
+++ src/app/common/alert-list/alert-list.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlertListComponent } from './alert-list.component';
+
+describe('NotFoundComponent', () => {
+  let component: AlertListComponent;
+  let fixture: ComponentFixture<AlertListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AlertListComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AlertListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
