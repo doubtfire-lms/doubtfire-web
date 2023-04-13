507fcbeccf9a8dbf0110942f8dc0fdf8dc0b117b
jakerenzella <j@kerenzella.com>
2023-04-13 16:31:49 +1000

    refactor: remove unused animation declaration

--- src/app/home/splash-screen/splash-screen.component.ts
+++ src/app/home/splash-screen/splash-screen.component.ts
@@ -6,17 +6,6 @@ import { GlobalStateService } from 'src/app/projects/states/index/global-state.s
   selector: 'splash-screen',
   templateUrl: './splash-screen.component.html',
   styleUrls: ['./splash-screen.component.scss'],
-  animations: [
-    trigger('simpleFadeAnimation', [
-      // the "in" style determines the "resting" state of the element when it is visible.
-      state('in', style({ opacity: 1 })),
-      // fade in when created. this could also be written as transition('void => *')
-      transition(':enter', [style({ opacity: 0 }), animate(500)]),
-
-      // fade out when destroyed. this could also be written as transition('void => *')
-      transition(':leave', animate(500, style({ opacity: 0 }))),
-    ]),
-  ],
 })
 export class SplashScreenComponent implements OnInit {
   constructor(private host: ElementRef<HTMLElement>, private globalState: GlobalStateService) {}
