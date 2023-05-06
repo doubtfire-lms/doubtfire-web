5709c72868126c2cde0796dfd983991c8b676b32
jakerenzella <j@kerenzella.com>
2023-05-06 16:32:04 +1000

    refactor: (WIP) add new paginator/teaching periods

--- src/app/doubtfire-angular.module.ts
+++ src/app/doubtfire-angular.module.ts
@@ -33,7 +33,7 @@ import { MatLegacySliderModule as MatSliderModule } from '@angular/material/lega
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatLegacyPaginatorModule as MatPaginatorModule } from '@angular/material/legacy-paginator';
 import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
 import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
 import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
