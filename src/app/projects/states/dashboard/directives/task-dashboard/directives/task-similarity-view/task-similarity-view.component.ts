de005ee98a270998e2175109783cab72b92e36f3
Andrew Cain <macite@gmail.com>
2023-05-15 22:18:16 +1000

    feat: add ability to update similarity flag

--- src/app/projects/states/dashboard/directives/task-dashboard/directives/task-similarity-view/task-similarity-view.component.ts
+++ src/app/projects/states/dashboard/directives/task-dashboard/directives/task-similarity-view/task-similarity-view.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, Inject, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { MatAccordion } from '@angular/material/expansion';
+import { alertService } from 'src/app/ajs-upgraded-providers';
 import { Task } from 'src/app/api/models/task';
 import { TaskSimilarity } from 'src/app/api/models/task-similarity';
+import { TaskSimilarityService } from 'src/app/api/services/task-similarity.service';
 
 @Component({
   selector: 'f-task-similarity-view',
@@ -12,6 +14,8 @@ export class TaskSimilarityViewComponent implements OnChanges {
   @Input() task: Task;
   @ViewChild(MatAccordion) accordion: MatAccordion;
   panelOpenState = false;
+
+  constructor(private taskSimilarityService: TaskSimilarityService, @Inject(alertService) private alerts: any) { }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.task && changes.task.currentValue && this.task?.id) {
@@ -25,5 +29,15 @@ export class TaskSimilarityViewComponent implements OnChanges {
   toggleFlag(e: Event, similarity: TaskSimilarity) {
     e.stopPropagation();
     similarity.flagged = !similarity.flagged;
+    this.taskSimilarityService
+      .update({ taskId: similarity.task.id, id: similarity.id }, { entity: similarity })
+      .subscribe((_) => {
+        this.alerts.add('success', 'Updated', 1500);
+        similarity.task.similarityFlag = similarity.task.similarityCache.currentValues
+          .map((s) => {
+            return s.flagged;
+          })
+          .reduce((a, b) => a || b, false);
+      });
   }
 }
