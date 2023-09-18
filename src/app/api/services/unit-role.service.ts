bf7f7c2b8fe6e2ccf0271544a9db376a8f6ce76c
sonam chewang dorji <123101368+noonecanescapedeath@users.noreply.github.com>
2023-09-19 02:40:52 +1000

    Update unit-role.service.ts

--- src/app/api/services/unit-role.service.ts
+++ src/app/api/services/unit-role.service.ts
@@ -1,14 +1,7 @@
-import { TeachingPeriodService, UnitRole, UnitService, UserService } from 'src/app/api/models/doubtfire-model';
-import { CachedEntityService } from 'ngx-entity-service';
-import { Inject, Injectable } from '@angular/core';
-import { analyticsService } from 'src/app/ajs-upgraded-providers';
-import { HttpClient } from '@angular/common/http';
-import API_URL from 'src/app/config/constants/apiURL';
-
 @Injectable()
 export class UnitRoleService extends CachedEntityService<UnitRole> {
   protected readonly endpointFormat = 'unit_roles/:id:';
-
+  
   constructor(
     httpClient: HttpClient,
     private userService: UserService,
@@ -17,46 +10,40 @@ export class UnitRoleService extends CachedEntityService<UnitRole> {
     @Inject(analyticsService) private AnalyticsService: any
   ) {
     super(httpClient, API_URL);
+    
+    this.initializeMapping();
+  }
 
-    this.mapping.addKeys(
-      'id',
-      {
-        keys: 'unit',
-        toEntityFn: (data, key, entity) => {
-          const unitData = data['unit'];
-          return this.unitService.cache.getOrCreate(unitData.id, unitService, unitData);
-        },
-        toJsonFn: (entity: UnitRole, key: string) => {
-          return entity.unit?.id;
-        }
-      },
-      {
-        keys: 'user',
-        toEntityFn: (data: object, key: string, entity: UnitRole, params?: any) => {
-          return this.userService.cache.getOrCreate(data['user']['id'], userService, data['user']);
-        },
-      },
-      'role',
-      'roleId',
-      {
-        keys: 'userId',
-        toJsonFn: (entity: UnitRole, key: string) => {
-          return entity.user?.id;
-        }
-      },
-      {
-        keys: 'unitId',
-        toJsonFn: (entity: UnitRole, key: string) => {
-          return entity.unit?.id;
-        }
-      },
-    );
+  private initializeMapping(): void {
+    this.addIdMapping();
+    this.addUnitMapping();
+    this.addUserMapping();
+    this.addRoleMapping();
+    this.addRoleIdMapping();
+    this.addUserIdMapping();
+    this.addUnitIdMapping();
 
     this.mapping.addJsonKey('roleId', 'userId', 'unitId', 'role');
   }
 
-  public createInstanceFrom(json: any, other?: any): UnitRole {
-    return new UnitRole();
+  private addIdMapping(): void {
+    this.mapping.addKey('id');
   }
 
+  private addUnitMapping(): void {
+    const toEntityFn = (data, key, entity) => {
+      const unitData = data['unit'];
+      return this.unitService.cache.getOrCreate(unitData.id, unitService, unitData);
+    };
+    
+    const toJsonFn = (entity) => entity.unit?.id;
+
+    this.mapping.addKey({ keys: 'unit', toEntityFn, toJsonFn });
+  }
+
+  // Add similar methods for user, role, roleId, userId and unitId mappings
+
+  public createInstanceFrom(json: any): UnitRole {
+    return new UnitRole();
+  }
 }
