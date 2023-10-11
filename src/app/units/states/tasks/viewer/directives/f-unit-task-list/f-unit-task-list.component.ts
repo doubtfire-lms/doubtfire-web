40e3ac3379e2b6fdfad842947c60dd67759931b7
John Van Pham <z3216645@unsw.edu.au>
2023-10-11 15:03:12 +1100

    refactor: remove old comments

--- src/app/units/states/tasks/viewer/directives/f-unit-task-list/f-unit-task-list.component.ts
+++ src/app/units/states/tasks/viewer/directives/f-unit-task-list/f-unit-task-list.component.ts
@@ -2,10 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Unit } from 'src/app/api/models/unit';
 import { Grade } from 'src/app/api/models/grade';
 import { Task, TaskDefinition } from 'src/app/api/models/doubtfire-model';
-import { Observable } from 'rxjs';
-import { SelectedTaskService } from 'src/app/projects/states/dashboard/selected-task.service';
 import { TaskDefinitionNamePipe } from 'src/app/common/filters/task-definition-name.pipe';
-
 
 @Component({
   selector: 'f-unit-task-list',
@@ -18,41 +15,22 @@ export class FUnitTaskListComponent implements OnInit {
   @Input() task: Task;
   @Input() selectedTaskDef: TaskDefinition;
 
-  @Input() taskData: {
-    source: (unit: Unit, taskDef: TaskDefinition | number) => Observable<Task[]>;
-    selectedTask: Task,
-    onSelectedTaskChange: (task: Task) => void;
-  }
-
-
-  // task filtering
-  //----------------------------------------------------------------------------------------------------
-
   filteredTasks: TaskDefinition[] = null; // list of tasks which match the taskSearch term
   taskSearch: string = "";  // task search term from user input
   taskDefinitionNamePipe = new TaskDefinitionNamePipe();
+  private gradeNames: string[] = Grade.GRADES;
+
+  constructor() {}
 
   applyFilters() {
     this.filteredTasks = this.taskDefinitionNamePipe.transform(this.unitTasks, this.taskSearch);
   }
 
-  //----------------------------------------------------------------------------------------------------
-
-  private gradeNames: string[] = Grade.GRADES;
-
-  constructor(
-    private selectedTaskService: SelectedTaskService
-  ) {}
-
   ngOnInit(): void {
-    // console.log(this.unit);
-
-    // Apply filters first-time
     this.applyFilters();
   }
 
   setSelectedTask(task: TaskDefinition) {
-    // console.log(task);
     this.selectedTaskDef = task;
   }
 
