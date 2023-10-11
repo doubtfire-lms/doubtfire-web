59c5dd363ca8dadf2e8d8bfc341431ba76106f43
Oscar Feng <feng.oscar@gmail.com>
2023-10-11 17:10:45 +1100

    refactor: create initial files for migration of task-details-view component

---
+++ src/app/units/states/tasks/viewer/directives/f-task-details-view/f-task-details-view.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'f-task-details-view',
+  templateUrl: 'f-task-details-view.component.html',
+  styleUrls: ['f-task-details-view.component.scss'],
+})
+export class TaskDetailsViewComponent {
+  constructor() {}
+}
