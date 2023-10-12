baf5ec033ee4bcab146d70a14b7cbc0d9bcc4dd3
Weizhi Chen <simonweizhi@gmail.com>
2023-10-12 18:18:11 +1100

    refactor: add f-units component

---
+++ src/app/admin/states/f-units/f-units.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Inject } from '@angular/core';
+import { createUnitModal } from 'src/app/ajs-upgraded-providers';
+
+
+
+@Component({
+  selector: 'f-units',
+  templateUrl: './f-units.component.html',
+  styleUrls: ['./f-units.component.scss'],
+})
+export class FUnitsComponent {
+  constructor(
+
+  ) {}
+}
