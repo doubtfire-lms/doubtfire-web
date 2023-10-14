df3fac3e3b685b8fc8fea5c3fd549db4bb5f2c45
Weizhi Chen <simonweizhi@gmail.com>
2023-10-14 23:08:26 +1100

    refactor: add unit loading and creation functionality for FUnitComponent

--- src/app/admin/states/f-units/f-units.component.ts
+++ src/app/admin/states/f-units/f-units.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, Input, Inject, OnInit } from '@angular/core';
 import { createUnitModal } from 'src/app/ajs-upgraded-providers';
-
+import { Unit } from 'src/app/api/models/unit';
+import { UnitRole } from 'src/app/api/models/unit-role';
+import { GlobalStateService, ViewType } from 'src/app/projects/states/index/global-state.service';
+import { UnitService } from 'src/app/api/services/unit.service';
 
 
 @Component({
@@ -8,8 +11,48 @@ import { createUnitModal } from 'src/app/ajs-upgraded-providers';
   templateUrl: './f-units.component.html',
   styleUrls: ['./f-units.component.scss'],
 })
-export class FUnitsComponent {
-  constructor(
+export class FUnitsComponent implements OnInit {
+  public allUnits: Unit[];
+  unitRoles: UnitRole[];
+  //dataload: boolean;
 
+  constructor(
+    @Inject(createUnitModal) private createUnitModal: any,
+    private globalStateService: GlobalStateService,
+    private unitService: UnitService
   ) {}
+
+  ngOnInit() {
+    this.globalStateService.setView(ViewType.OTHER);
+
+
+
+    // Listen for units to be loaded
+    this.globalStateService.onLoad(() => {
+      const loadedUnitRoles = this.globalStateService.loadedUnitRoles.currentValues;
+      this.unitRoles = [...loadedUnitRoles];
+
+      this.globalStateService.loadedUnits.values.subscribe((units) => (this.allUnits = units));
+      this.loadAllUnits();
+    });
+
+
+  }
+
+  createUnit() {
+    this.createUnitModal.show(this.allUnits);
+  }
+
+  private loadAllUnits() {
+    // Load all units
+    this.unitService.query(undefined, { params: { include_in_active: true } }).subscribe({
+      next: (success) => {
+        return;
+      },
+      error: (failure) => {
+        //TODO: Add alert
+        console.log(failure);
+      },
+    });
+  }
 }
