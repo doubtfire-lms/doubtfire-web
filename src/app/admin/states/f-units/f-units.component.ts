4c2b13adf6efe22893e3451db6ec0240996ad4d8
Weizhi Chen <simonweizhi@gmail.com>
2023-10-18 17:40:36 +1100

    refactor: add search, router and filter functionality

--- src/app/admin/states/f-units/f-units.component.ts
+++ src/app/admin/states/f-units/f-units.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input, Inject, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router'; // Import at the top
+import { UIRouter } from '@uirouter/core';
 import { createUnitModal } from 'src/app/ajs-upgraded-providers';
 import { Unit } from 'src/app/api/models/unit';
 import { UnitRole } from 'src/app/api/models/unit-role';
 import { GlobalStateService, ViewType } from 'src/app/projects/states/index/global-state.service';
 import { UnitService } from 'src/app/api/services/unit.service';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
-import { UIRouter } from '@uirouter/angular';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
@@ -17,6 +16,7 @@ import { MatPaginator } from '@angular/material/paginator';
 })
 
 export class FUnitsComponent implements AfterViewInit, OnInit {
+
   @ViewChild(MatTable, { static: false }) table: MatTable<Unit>;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
@@ -37,14 +37,17 @@ export class FUnitsComponent implements AfterViewInit, OnInit {
   unitRoles: UnitRole[];
   dataload: boolean;
 
+
+
   constructor(
     @Inject(createUnitModal) private createUnitModal: any,
     private globalStateService: GlobalStateService,
     private unitService: UnitService,
-    private routers: Router, // Inject the Angular router
+    private router: UIRouter
 
-    private router: UIRouter,
+
   ) {
+
     this.dataload = false;
   }
 
@@ -57,11 +60,7 @@ export class FUnitsComponent implements AfterViewInit, OnInit {
     }
   }
 
-  navigateToDetails(unit: Unit) {
-    // Assuming you want to navigate to a 'details' route with the unit's id as a parameter
-    this.routers.navigate(['/units', unit.id, 'admin']);
-    console.log(`/#/units/${unit.id}/admin`);
-  }
+
 
   ngOnInit() {
     this.globalStateService.setView(ViewType.OTHER);
@@ -73,7 +72,7 @@ export class FUnitsComponent implements AfterViewInit, OnInit {
 
       this.globalStateService.loadedUnits.values.subscribe((units) => (this.allUnits = units));
       this.loadAllUnits();
-      console.log(this.dataSource);
+      //console.log(this.dataSource);
     });
     this.loadAllUnits();
   }
@@ -96,4 +95,41 @@ export class FUnitsComponent implements AfterViewInit, OnInit {
       },
     });
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  private sortCompare(aValue: number | string, bValue: number | string, isAsc: boolean) {
+    return (aValue < bValue ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+  // Sorting function to sort data when sort
+  // event is triggered
+  sortTableData(sort: Sort) {
+    if (!sort.active || sort.direction === '') {
+      return;
+    }
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'unit_code':
+        case 'name':
+        case 'unit_role':
+        case 'teaching_period':
+        case 'start_date':
+        case 'end_date':
+        case 'active':
+          return this.sortCompare(a[sort.active], b[sort.active], isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+
 }
