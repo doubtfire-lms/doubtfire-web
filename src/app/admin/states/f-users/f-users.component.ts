9b0e4c17338fcba8a8bf45ae04e1f3f98c5eb3e3
John Van Pham <john.vn.pham@gmail.com>
2023-10-24 20:03:13 +1100

    refactor: migrate users to f-users (wip)

---
+++ src/app/admin/states/f-users/f-users.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'f-users',
+  templateUrl: './f-users.component.html',
+  styleUrls: ['./f-users.component.scss'],
+})
+export class FUsersComponent {
+  constructor() {}
+}
