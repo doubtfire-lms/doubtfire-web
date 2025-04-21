b06e5936bc2d30f5c01a15dda0ea6a3fac91cc18
Martin Dolores <martindolores65@gmail.com>
2025-04-21 18:49:10 +1000

    - remove unused reference

--- src/app/common/content-editable/content-editable.directive.ts
+++ src/app/common/content-editable/content-editable.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2, HostListener, Input, forwardRef} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, forwardRef} from '@angular/core';
 import {NG_VALUE_ACCESSOR, ControlValueAccessor} from '@angular/forms';
 
 @Directive({
