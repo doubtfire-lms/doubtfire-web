c8dd020a5e572765f7710c266ae2b20563ab05c2
b0ink <40929320+b0ink@users.noreply.github.com>
2025-07-23 15:53:42 +1000

    feat: ensure complete button updates aip only tasks to working on it

--- src/app/projects/states/tutor-discussion/tutor-discussion.component.ts
+++ src/app/projects/states/tutor-discussion/tutor-discussion.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, Input, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, Component, Input, ViewChild, ViewEncapsulation} from '@angular/core';
 import {MatSelectionList} from '@angular/material/list';
 import {MatTabChangeEvent} from '@angular/material/tabs';
 import {StateService, UIRouter} from '@uirouter/core';
@@ -206,7 +206,11 @@ export class TutorDiscussionComponent implements AfterViewInit {
     const selectedTasks = this.tasksList.selectedOptions.selected;
     for (const taskOption of selectedTasks) {
       const task = taskOption.value as Task;
-      task.updateTaskStatus(status, true);
+      if (task.definition.assessInPortfolioOnly) {
+        task.updateTaskStatus(status === 'complete' ? 'working_on_it' : status, true);
+      } else {
+        task.updateTaskStatus(status, true);
+      }
     }
   }
 
