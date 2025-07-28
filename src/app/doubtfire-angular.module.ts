04a40a5668de492a6546f2728d6a9a4904395eb6
b0ink <40929320+b0ink@users.noreply.github.com>
2025-07-28 11:14:56 +1000

    refactor: move unavailable state to component

--- src/app/doubtfire-angular.module.ts
+++ src/app/doubtfire-angular.module.ts
@@ -271,6 +271,7 @@ import {StaffNotesComponent} from './projects/states/staff-notes/staff-notes.com
 import {StaffNoteService} from './api/services/staff-note.service';
 import {LocalizedDatePipe} from './common/pipes/localized-date.pipe';
 import {StaffNotesViewComponent} from './projects/states/dashboard/directives/task-dashboard/directives/staff-notes-view/staff-notes-view.component';
+import {UnavailableCardComponent} from './errors/unavailable-card/unavailable-card.component';
 
 // See https://stackoverflow.com/questions/55721254/how-to-change-mat-datepicker-date-format-to-dd-mm-yyyy-in-simplest-way/58189036#58189036
 const MY_DATE_FORMAT = {
@@ -410,6 +411,7 @@ const MY_DATE_FORMAT = {
     StaffNotesViewComponent,
     QrModalComponent,
     LocalizedDatePipe,
+    UnavailableCardComponent,
   ],
   // Services we provide
   providers: [
