1d42c139f50b0d265f702ebf24cd6e36840401f4
b0ink <40929320+b0ink@users.noreply.github.com>
2025-08-06 16:57:28 +1000

    chore: init placeholder ui

--- src/app/home/states/lti-deeplink/lti-deeplink.component.ts
+++ src/app/home/states/lti-deeplink/lti-deeplink.component.ts
@@ -1,5 +1,7 @@
-import {AfterViewInit, Component, Inject} from '@angular/core';
+import {AfterViewInit, Component, Inject, Input} from '@angular/core';
 import {UIRouter} from '@uirouter/angular';
+import {Subscription} from 'rxjs';
+import {CreateNewUnitModal} from 'src/app/admin/modals/create-new-unit-modal/create-new-unit-modal.component';
 
 @Component({
   selector: 'f-lti-deeplink',
@@ -7,17 +9,31 @@ import {UIRouter} from '@uirouter/angular';
   styleUrls: ['lti-deeplink.component.scss'],
 })
 export class LtiDeeplinkComponent implements AfterViewInit {
-  constructor(@Inject(UIRouter) private router: UIRouter) {}
+  constructor(private createUnitModalService: CreateNewUnitModal) {}
 
   selectedUnit: string;
+
+  @Input() ltiKey: string;
 
   ngAfterViewInit(): void {
     // Scroll to the bottom of the page in case the header is visible
     // Ensures our action buttons are centered
     setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 100);
+
+    console.log(this.ltiKey);
   }
 
   public submit(): void {
     console.log(`Selected unit is ${this.selectedUnit}`);
+  }
+
+  public createUnit(): void {
+    const dialog = this.createUnitModalService.show();
+
+    const sub = dialog.afterAllClosed.subscribe(() => {
+      console.log('closed! fetching the units again...');
+      // Try defaulting to the last unit created?
+      sub.unsubscribe();
+    });
   }
 }
