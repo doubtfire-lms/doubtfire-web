c6e4223064abe7b60596f6561009c4b43814407b
b0ink <40929320+b0ink@users.noreply.github.com>
2025-08-13 14:46:49 +1000

    refactor: move csv downloads into sidekiq jobs

--- src/app/units/states/analytics/unit-analytics-route.component.ts
+++ src/app/units/states/analytics/unit-analytics-route.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
-import { Unit } from 'src/app/api/models/unit';
+import {Component, Input} from '@angular/core';
+import {Observable} from 'rxjs';
+import {SidekiqJob} from 'src/app/api/models/sidekiq-job';
+import {Unit} from 'src/app/api/models/unit';
+import {FileDownloaderService} from 'src/app/common/file-downloader/file-downloader.service';
+import {SidekiqProgressModalService} from 'src/app/common/modals/sidekiq-progress-modal/sidekiq-progress-modal.service';
+import {AlertService} from 'src/app/common/services/alert.service';
 
 @Component({
   selector: 'f-unit-analytics',
@@ -8,4 +13,46 @@ import { Unit } from 'src/app/api/models/unit';
 })
 export class UnitAnalyticsComponent {
   @Input() unit: Unit;
+
+  constructor(
+    private sidekiqProgressModalService: SidekiqProgressModalService,
+    private alertsService: AlertService,
+    private fileDownloaderService: FileDownloaderService,
+  ) {}
+
+  public getTaskCompletionCsv() {
+    this.downloadCsv(
+      this.unit.downloadTaskCompletionCsv(),
+      'Task Completion Stats CSV',
+      `${this.unit.code}-task-completion-stats.csv`,
+    );
+  }
+
+  public getTutorAssessmentCsv() {
+    this.downloadCsv(
+      this.unit.downloadTutorAssessmentCsv(),
+      'Tutor Assessment Stats CSV',
+      `${this.unit.code}-tutor-assessment-stats.csv`,
+    );
+  }
+
+  private downloadCsv(newJob: Observable<SidekiqJob>, title: string, filename: string) {
+    newJob.subscribe({
+      next: (job) => {
+        if (!job || !job.id) {
+          return this.alertsService.error(`Failed to download ${title}`, 6000);
+        }
+        this.sidekiqProgressModalService.show(`Downloading ${title}`, job.id).subscribe((job) => {
+          console.log(job);
+          const blob = new Blob([job.result], {type: 'text/csv'});
+          const url = URL.createObjectURL(blob);
+
+          this.fileDownloaderService.downloadBlobToFile(url, filename);
+        });
+      },
+      error: (_error) => {
+        this.alertsService.error(`Could not download ${title}`, 6000);
+      },
+    });
+  }
 }
