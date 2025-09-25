db1ad9b8b6bde544d1e1dfc9cbe3fe3b5c79fff5
Boink <40929320+b0ink@users.noreply.github.com>
2025-09-25 12:42:34 +1000

    refactor: migrate unit details editor (#1007)
    * refactor: init unit details editor migration
    
    * refactor: migrate unit details editor
    
    * refactor: migrate assess in portfolio confirmation
    
    * refactor: display teaching period dates when selected
    
    * refactor: add draft learning summary hint
    
    * chore: remove old unit details editor files
    
    * chore: remove debug logs

--- src/app/doubtfire-angular.module.ts
+++ src/app/doubtfire-angular.module.ts
@@ -288,6 +288,7 @@ import {TaskPrerequisiteService} from './api/services/task-prerequisite.service'
 // import { PrivacyPolicy } from './config/privacy-policy/privacy-policy';
 import {UnitStaffEditorComponent} from './units/states/edit/directives/unit-staff-editor/unit-staff-editor.component';
 import {GroupSetSelectorComponent} from './groups/group-set-selector/group-set-selector.component';
+import {UnitDetailsEditorComponent} from './units/states/edit/directives/unit-details-editor/unit-details-editor.component';
 
 // See https://stackoverflow.com/questions/55721254/how-to-change-mat-datepicker-date-format-to-dd-mm-yyyy-in-simplest-way/58189036#58189036
 const MY_DATE_FORMAT = {
@@ -438,6 +439,7 @@ const MY_DATE_FORMAT = {
     TaskPrerequisitesCardComponent,
     UnitStaffEditorComponent,
     GroupSetSelectorComponent,
+    UnitDetailsEditorComponent,
   ],
   // Services we provide
   providers: [
