6dc64d4448b4e5d81c694b15ffe2d7ad4a2a9cde
b0ink <40929320+b0ink@users.noreply.github.com>
2025-10-03 12:21:58 +1000

    refactor: improve tutorials component and add sorting
    - fetch unit to ensure tutorials are loaded correctly
    - add table sorting

--- src/app/projects/states/tutorials/tutorials.component.ts
+++ src/app/projects/states/tutorials/tutorials.component.ts
@@ -1,12 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {StateService} from '@uirouter/angular';
-import {ProjectService} from 'src/app/api/services/project.service';
+import {Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {Tutorial, UnitService} from 'src/app/api/models/doubtfire-model';
 import {Project} from 'src/app/api/models/project';
 import {Unit} from 'src/app/api/models/unit';
-import {Tutorial} from 'src/app/api/models/doubtfire-model';
+import {ProjectService} from 'src/app/api/services/project.service';
 
 @Component({
-  selector: 'tutorials',
+  selector: 'f-tutorials',
   templateUrl: './tutorials.component.html',
   styleUrls: ['./tutorials.component.scss'],
 })
@@ -14,96 +15,47 @@ export class TutorialsComponent implements OnInit {
   @Input() projectId: number;
 
   filteredTutorials: Tutorial[] = [];
-  loading: boolean = true; // Used to handle the async fetching of data.
-  pageTitle: string = '';
+
   project: Project;
-  reverse: boolean = false;
-  roleWhiteList: string[] = [];
-  sortOrder: string = ''; // Default sort order.
-  task: string = '';
   unit: Unit;
 
+  displayedColumns: string[] = [
+    'stream',
+    'campus',
+    'code',
+    'day',
+    'time',
+    'room',
+    'tutor',
+    'actions',
+  ];
+
+  dataSource = new MatTableDataSource<Tutorial>([]);
+
   constructor(
-    private stateService: StateService,
     private projectService: ProjectService,
+    private unitService: UnitService,
   ) {}
 
   ngOnInit(): void {
     this.projectService.fetch(this.projectId).subscribe({
       next: (project) => {
-        this.project = project;
-        this.unit = project.unit;
-        this.filteredTutorials = this.tutorialCampusFilter(
-          [...project.unit.tutorials],
-          this.project,
-        );
-
-        // Set the sort order based on the unit's tutorialStreamsCache size
-        if (this.unit?.tutorialStreamsCache?.size > 0) {
-          this.sortOrder = 'tutorialStream.name';
-        } else {
-          this.sortOrder = 'abbreviation';
-        }
-
-        // Access the `data` property of the current state (UI-Router)
-        const currentState = this.stateService.current;
-        this.pageTitle = currentState.data.pageTitle;
-        this.task = currentState.data.task;
-        this.roleWhiteList = currentState.data.roleWhiteList;
-
-        // Mark loading as complete - we can now render the template
-        this.loading = false;
+        this.unitService.get(project.unit.id).subscribe({
+          next: (unit) => {
+            this.unit = unit;
+            this.project = project;
+            this.filteredTutorials = this.tutorialCampusFilter([...unit.tutorials], this.project);
+            this.dataSource.data = this.filteredTutorials;
+          },
+          error: (error) => {
+            console.error('Error fetching unit:', error);
+          },
+        });
       },
       error: (error) => {
         console.error('Error fetching project:', error);
-        this.loading = false; // Ensure loading is stopped even on error
       },
     });
-  }
-
-  /**
-   * Used to determine how to sort the table view.
-   *
-   * @param order
-   *
-   * @returns void
-   */
-  setSortOrder(order: string): void {
-    if (this.sortOrder === order) {
-      this.reverse = !this.reverse;
-    } else {
-      this.sortOrder = order;
-      this.reverse = false;
-    }
-    this.updateFilteredTutorials();
-  }
-
-  /**
-   * Used to perform the actual sorting of the data in the table view.
-   *
-   * @returns void
-   */
-  updateFilteredTutorials(): void {
-    this.filteredTutorials = [...this.filteredTutorials].sort((a, b) => {
-      const valueA = this.getValueByPath(a, this.sortOrder);
-      const valueB = this.getValueByPath(b, this.sortOrder);
-
-      if (valueA < valueB) return this.reverse ? 1 : -1;
-      if (valueA > valueB) return this.reverse ? -1 : 1;
-      return 0;
-    });
-  }
-
-  /**
-   * Traverses the passed-in object to find the key at the supplied path.
-   *
-   * @param obj Object to traverse
-   * @param path The path of the object to find
-   *
-   * @returns any
-   */
-  getValueByPath(obj: object, path: string): any {
-    return path.split('.').reduce((acc, part) => acc && acc[part], obj);
   }
 
   /**
@@ -115,17 +67,6 @@ export class TutorialsComponent implements OnInit {
    */
   switchToTutorial(tutorial: Tutorial): void {
     this.project.switchToTutorial(tutorial);
-  }
-
-  /**
-   * Determines whether the user is enrolled in the passed-in tutorial.
-   *
-   * @param tutorial
-   *
-   * @returns boolean
-   */
-  isEnrolledIn(tutorial: Tutorial): boolean {
-    return this.project.isEnrolledIn(tutorial);
   }
 
   /**
@@ -163,5 +104,46 @@ export class TutorialsComponent implements OnInit {
     const formattedMinutes = minutes.padStart(2, '0');
 
     return `${formattedHours}:${formattedMinutes}`;
+  }
+
+  private sortCompare(aValue: number | string, bValue: number | string, isAsc: boolean) {
+    return (aValue < bValue ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+  sortTableData(sort: Sort) {
+    if (!sort.active || sort.direction === '') {
+      return;
+    }
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      switch (sort.active) {
+        case 'stream':
+          return this.sortCompare(
+            a.tutorialStream?.name,
+            b.tutorialStream?.name,
+            sort.direction === 'asc',
+          );
+        case 'campus':
+          return this.sortCompare(a.campus?.name, b.campus?.name, sort.direction === 'asc');
+        case 'code':
+          return this.sortCompare(a.abbreviation, b.abbreviation, sort.direction === 'asc');
+        case 'day': {
+          return this.sortCompare(a.meetingDay, b.meetingDay, sort.direction === 'asc');
+        }
+        case 'time': {
+          return this.sortCompare(
+            this.shortTime(a.meetingTime),
+            this.shortTime(b.meetingTime),
+            sort.direction === 'asc',
+          );
+        }
+        case 'room': {
+          return this.sortCompare(a.meetingLocation, b.meetingLocation, sort.direction === 'asc');
+        }
+        case 'tutor':
+          return this.sortCompare(a.tutorName, b.tutorName, sort.direction === 'asc');
+        default:
+          return 0;
+      }
+    });
   }
 }
