10e8ab033469c2dd4e78d125cb8fcfe0127cbf88
Boink <40929320+b0ink@users.noreply.github.com>
2025-10-03 14:55:31 +1000

    refactor: migrate portfolio grade select step (#1014)
    * Frontend migration of portfolio-grade-select-step to Angular 17 component
    
    * Addressed review comments in portfolio-grade-select-step component
    
    * Implement OnInit in PortfolioGradeSelectStepComponent
    
    * replaced bootsrap UI component with Angular material
    
    * chore: bring back deleted lines
    
    * refactor: replace css with tailwind
    
    * chore: revert import order
    
    * chore: revert import order
    
    * chore: revert import order
    
    * chore: revert order
    
    * refactor: simplify grade changing
    
    ---------
    
    Co-authored-by: Pasindu Fernando <116358471+Pasindufdo98@users.noreply.github.com>

--- src/app/doubtfire-angular.module.ts
+++ src/app/doubtfire-angular.module.ts
@@ -283,12 +283,12 @@ import {LtiService} from './api/services/lti.service';
 import {TaskDefinitionPrerequisitesComponent} from './units/states/edit/directives/unit-tasks-editor/task-definition-editor/task-definition-prerequisites/task-definition-prerequisites.component';
 import {TaskPrerequisitesCardComponent} from './projects/states/dashboard/directives/task-dashboard/directives/task-prerequisites-card/task-prerequisites-card.component';
 import {TaskPrerequisiteService} from './api/services/task-prerequisite.service';
-// import { GradeIconComponent } from './common/grade-icon/grade-icon.component';
-// import { GradeTaskModalComponent } from './tasks/modals/grade-task-modal/grade-task-modal.component';
-// import { PrivacyPolicy } from './config/privacy-policy/privacy-policy';
+// import {GradeTaskModalComponent} from './tasks/modals/grade-task-modal/grade-task-modal.component';
+// import {PrivacyPolicy} from './config/privacy-policy/privacy-policy';
 import {UnitStaffEditorComponent} from './units/states/edit/directives/unit-staff-editor/unit-staff-editor.component';
 import {GroupSetSelectorComponent} from './groups/group-set-selector/group-set-selector.component';
 import {UnitDetailsEditorComponent} from './units/states/edit/directives/unit-details-editor/unit-details-editor.component';
+import {PortfolioGradeSelectStepComponent} from './projects/states/portfolio/directives/portfolio-grade-select-step/portfolio-grade-select-step.component';
 
 // See https://stackoverflow.com/questions/55721254/how-to-change-mat-datepicker-date-format-to-dd-mm-yyyy-in-simplest-way/58189036#58189036
 const MY_DATE_FORMAT = {
@@ -440,6 +440,7 @@ const MY_DATE_FORMAT = {
     UnitStaffEditorComponent,
     GroupSetSelectorComponent,
     UnitDetailsEditorComponent,
+    PortfolioGradeSelectStepComponent,
   ],
   // Services we provide
   providers: [
