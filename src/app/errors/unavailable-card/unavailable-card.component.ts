f7f76ac6f42d745e9034033c2e93c182f58b5c4e
b0ink <40929320+b0ink@users.noreply.github.com>
2025-10-13 12:04:04 +1100

    feat: auto refresh once api is running again

--- src/app/errors/unavailable-card/unavailable-card.component.ts
+++ src/app/errors/unavailable-card/unavailable-card.component.ts
@@ -1,8 +1,28 @@
-import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {DoubtfireConstants} from 'src/app/config/constants/doubtfire-constants';
 
 @Component({
   selector: 'f-unavailable-card',
   templateUrl: './unavailable-card.component.html',
   styleUrls: ['./unavailable-card.component.scss'],
 })
-export class UnavailableCardComponent {}
+export class UnavailableCardComponent implements OnInit {
+  constructor(
+    private httpClient: HttpClient,
+    private constants: DoubtfireConstants,
+  ) {}
+
+  private attemptRefresh = null;
+
+  ngOnInit() {
+    clearInterval(this.attemptRefresh);
+    this.attemptRefresh = setInterval(() => {
+      this.httpClient.get(`${this.constants.API_URL}/auth/method`).subscribe(() => {
+        window.location.reload();
+        clearInterval(this.attemptRefresh);
+        this.attemptRefresh = null;
+      });
+    }, 5000);
+  }
+}
