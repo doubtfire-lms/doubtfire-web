b7ac359792c37549b89804e4bb9bee4a26c30dc0
b0ink <40929320+b0ink@users.noreply.github.com>
2025-10-29 08:33:12 +1100

    feat: restrict assessments to tutors in the same tutorial stream

--- src/app/api/models/task-definition.ts
+++ src/app/api/models/task-definition.ts
@@ -55,6 +55,7 @@ export class TaskDefinition extends Entity {
   hasJplagReport: boolean;
   assessInPortfolioOnly: boolean;
   useResourcesForJplagBaseCode: boolean;
+  lockAssessmentsToTutorialStream: boolean;
 
   public readonly taskPrerequisitesCache: EntityCache<TaskPrerequisite> =
     new EntityCache<TaskPrerequisite>();
--- src/app/api/services/task-definition.service.ts
+++ src/app/api/services/task-definition.service.ts
@@ -137,6 +137,7 @@ export class TaskDefinitionService extends CachedEntityService<TaskDefinition> {
         },
       },
       'useResourcesForJplagBaseCode',
+      'lockAssessmentsToTutorialStream',
     );
 
     this.mapping.mapAllKeysToJsonExcept(
