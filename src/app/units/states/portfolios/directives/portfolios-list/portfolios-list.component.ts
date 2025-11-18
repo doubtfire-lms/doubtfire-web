8827dce85b25cebc3e6deae31ad757afc8035602
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-18 17:03:19 +1100

    refactor: migrate select student portfolio list

--- src/app/units/states/portfolios/directives/portfolios-list/portfolios-list.component.ts
+++ src/app/units/states/portfolios/directives/portfolios-list/portfolios-list.component.ts
@@ -1,8 +1,83 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {Project} from 'src/app/api/models/project';
+import {TaskStatusEnum} from 'src/app/api/models/task-status';
+import {Unit} from 'src/app/api/models/unit';
+import {TaskService} from 'src/app/api/services/task.service';
 
 @Component({
   selector: 'f-portfolios-list',
   templateUrl: './portfolios-list.component.html',
   styleUrl: './portfolios-list.component.scss',
 })
-export class PortfoliosListComponent {}
+export class PortfoliosListComponent implements OnInit {
+  constructor(private taskService: TaskService) {}
+  @Input() unit: Unit;
+
+  displayedColumns: string[] = [
+    'student',
+    'name',
+    'tutor',
+    'tutorial',
+    'target',
+    'submitted-as',
+    'stats',
+    'grade',
+  ];
+
+  dataSource = new MatTableDataSource<Project>([]);
+
+  public statusColor(status: TaskStatusEnum): string {
+    return this.taskService.statusColors.get(status);
+  }
+
+  public statusLabel(status: TaskStatusEnum): string {
+    return this.taskService.statusLabels.get(status);
+  }
+
+  ngOnInit(): void {
+    console.log(this.unit);
+    this.dataSource.data = [...this.unit.students];
+  }
+
+  private sortCompare(aValue: number | string, bValue: number | string, isAsc: boolean) {
+    return (aValue < bValue ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+  sortTableData(sort: Sort) {
+    if (!sort.active || sort.direction === '') {
+      return;
+    }
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      switch (sort.active) {
+        case 'student':
+          return this.sortCompare(
+            a.student.studentId || a.student.username,
+            b.student.studentId || b.student.username,
+            sort.direction === 'asc',
+          );
+        case 'name':
+          return this.sortCompare(a.student?.name, b.student?.name, sort.direction === 'asc');
+        case 'tutor': {
+          return this.sortCompare(a.tutorNames(), b.tutorNames(), sort.direction === 'asc');
+        }
+        case 'tutorial':
+          return this.sortCompare(
+            a.shortTutorialDescription(),
+            b.shortTutorialDescription(),
+            sort.direction === 'asc',
+          );
+
+        case 'target':
+          return this.sortCompare(a.targetGrade, b.targetGrade, sort.direction === 'asc');
+        case 'submitted-as':
+          return this.sortCompare(a.submittedGrade, b.submittedGrade, sort.direction === 'asc');
+        case 'grade':
+          return this.sortCompare(a.grade, b.grade, sort.direction === 'asc');
+        default:
+          return 0;
+      }
+    });
+  }
+}
--- src/app/units/states/portfolios/portfolios.component.ts
+++ src/app/units/states/portfolios/portfolios.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Project} from 'src/app/api/models/project';
 import {Unit} from 'src/app/api/models/unit';
+import {ProjectService} from 'src/app/api/services/project.service';
 import {UnitService} from 'src/app/api/services/unit.service';
 import {GlobalStateService, ViewType} from 'src/app/projects/states/index/global-state.service';
 
@@ -14,17 +16,29 @@ export class PortfoliosComponent implements OnInit {
 
   // Exposed to child components
   public unit: Unit = null;
+  public selectedProject: Project;
 
   constructor(
     private globalStateService: GlobalStateService,
     private unitService: UnitService,
+    private projectService: ProjectService,
   ) {}
 
   ngOnInit(): void {
     this.unitService.get(this.unitId).subscribe({
       next: (unit) => {
-        this.unit = unit;
-        this.globalStateService.setView(ViewType.UNIT, this.unit);
+        this.globalStateService.setView(ViewType.UNIT, unit);
+
+        this.projectService.loadStudents(unit, false).subscribe({
+          next: () => {
+            console.log('got students');
+            this.unit = unit;
+          },
+          error: (error) => {
+            // TODO: redirect back to home..
+            console.error(error);
+          },
+        });
       },
       error: (error) => {
         console.error(error);
