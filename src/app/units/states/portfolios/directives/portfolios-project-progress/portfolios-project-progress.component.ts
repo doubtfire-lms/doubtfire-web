2e0250c1e391581b0155d4c3ba4eb60d4c10a654
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-19 10:34:07 +1100

    refactor: add target and submitted grade card ui

--- src/app/units/states/portfolios/directives/portfolios-project-progress/portfolios-project-progress.component.ts
+++ src/app/units/states/portfolios/directives/portfolios-project-progress/portfolios-project-progress.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {Project} from 'src/app/api/models/project';
 import {Unit} from 'src/app/api/models/unit';
+import {ProjectService} from 'src/app/api/services/project.service';
+import {AlertService} from 'src/app/common/services/alert.service';
+import {GradeService} from 'src/app/common/services/grade.service';
 
 @Component({
   selector: 'f-portfolios-project-progress',
@@ -10,4 +13,50 @@ import {Unit} from 'src/app/api/models/unit';
 export class PortfoliosProjectProgressComponent {
   @Input() project: Project;
   @Input() unit: Unit;
+
+  constructor(
+    private gradeService: GradeService,
+    private projectService: ProjectService,
+    private alertService: AlertService,
+  ) {}
+
+  public get grades() {
+    return this.gradeService.gradeValues;
+  }
+
+  public gradeWord(grade) {
+    return this.gradeService.grades[grade];
+  }
+
+  updateSubmittedGrade(newGrade: number): void {
+    const previousSubmittedGrade = this.project.submittedGrade;
+    this.project.submittedGrade = newGrade;
+
+    this.projectService.update(this.project).subscribe({
+      next: (project) => {
+        project.refreshBurndownChartData?.();
+        this.alertService.success(`Updated project's submitted grade`);
+      },
+      error: (error) => {
+        this.project.submittedGrade = previousSubmittedGrade;
+        this.alertService.error(`Failed to update submitted grade: ${error}`);
+      },
+    });
+  }
+
+  updatedTargetGrade(newGrade: number): void {
+    const previousTargetGrade = this.project.targetGrade;
+    this.project.targetGrade = newGrade;
+
+    this.projectService.update(this.project).subscribe({
+      next: (project) => {
+        project.refreshBurndownChartData?.();
+        this.alertService.success(`Updated project's target grade`);
+      },
+      error: (error) => {
+        this.project.targetGrade = previousTargetGrade;
+        this.alertService.error(`Failed to update target grade: ${error}`);
+      },
+    });
+  }
 }
