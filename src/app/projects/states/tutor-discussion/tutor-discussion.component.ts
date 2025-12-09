dba0522fce970a66b15fa574d53eecfab444ece2
b0ink <40929320+b0ink@users.noreply.github.com>
2025-12-09 11:17:58 +1100

    refactor: add warning badge to discuss check tasks

--- src/app/projects/states/tutor-discussion/tutor-discussion.component.ts
+++ src/app/projects/states/tutor-discussion/tutor-discussion.component.ts
@@ -352,6 +352,7 @@ export class TutorDiscussionComponent implements AfterViewInit {
     'demonstrate',
     'ready_for_feedback',
     'discuss',
+    'discuss_check',
     'need_help',
     // 'complete',
     'fix_and_resubmit',
