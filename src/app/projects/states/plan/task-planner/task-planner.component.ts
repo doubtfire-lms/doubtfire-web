6980696a41b6f92868d2b7af3be8ad5a3f9aa913
b0ink <40929320+b0ink@users.noreply.github.com>
2025-12-16 10:04:37 +1100

    refactor: remove unused code

--- src/app/projects/states/plan/task-planner/task-planner.component.ts
+++ src/app/projects/states/plan/task-planner/task-planner.component.ts
@@ -53,9 +53,7 @@ export class TaskPlannerComponent implements OnInit {
   }
 
   constructor(
-    private globalStateService: GlobalStateService,
     private gradeService: GradeService,
-    private projectService: ProjectService,
     private alertService: AlertService,
     private confirmationModalService: ConfirmationModalService,
     private taskPlannerPrerequisitesModal: TaskPlannerPrerequisitesModalService,
@@ -79,10 +77,6 @@ export class TaskPlannerComponent implements OnInit {
   }
 
   public showDatesColumn: boolean = false;
-
-  dragMoved(event: GanttDragEvent) {
-    // console.log(event);
-  }
 
   public overlayLines: boolean = false;
 
@@ -184,42 +178,7 @@ export class TaskPlannerComponent implements OnInit {
     this.allTaskPrerequisites = [...this.allTaskPrerequisites];
   }
 
-  dragEnded(event: GanttDragEvent) {
-    const item = event.item;
-    const td = this.getTaskDefinition(item.id);
-
-    const requisites = this.taskPrerequisites.filter((p) => p.prerequisiteId === td.id);
-    const requisiteItems = this.items.filter((p) =>
-      requisites.find((pre) => pre.taskDefinitionId == Number(p.id)),
-    );
-
-    // if (
-    //   requisiteItems.length &&
-    //   (this.isCloseToFeedbackDeadline(item) || this.isPastFeedbackDeadline(item))
-    // ) {
-    //   const task = this.project.findTaskForDefinition(td.id);
-
-    //   item.start = this.normalizeDateUTC(task.startDate.getTime() / 1000);
-    //   item.end = this.normalizeDateUTC(task.localDueDate().getTime() / 1000);
-
-    //   // If the task defaults are still invalid, reset them to the task definition default
-    //   if (this.isCloseToFeedbackDeadline(item) || this.isPastFeedbackDeadline(item)) {
-    //     item.start = this.normalizeDateUTC(td.startDate.getTime() / 1000);
-    //     item.end = this.normalizeDateUTC(td.localDueDate().getTime() / 1000);
-    //   }
-
-    //   this.items = [...this.items];
-    //   this.alertService.error(
-    //     `You must allow enough time to submit this task because it is a prerequisite for tasks: ${requisiteItems.map((r) => this.getTaskDefinition(r.id).abbreviation).join(', ')}`,
-    //     8000,
-    //   );
-    // }
-  }
-
   public blockedDependents: Map<string, boolean> = new Map();
-
-  // TODO: create a map to track errors for each task, and what their final color should currently be
-  // a task can hold multiple errors, for example
 
   // Check to see if this task is a prerequisite for another task
   // If it is, ensure the end date on the task is before the start of its dependent task
@@ -240,55 +199,31 @@ export class TaskPlannerComponent implements OnInit {
       }
       const diff = this.normalizeDateUTC(item.end) - this.normalizeDateUTC(ganttItem.end);
       const color = typeof ganttLink.color === 'string' ? ganttLink.color : ganttLink.color.default;
-      // this.blockedDependents.set(ganttItem.id, false);
-      // console.log(this.taskDefs().find((t) => t.id == 75));
+
       if (diff > 0) {
         isAfterDependentStartDate = true;
       }
+
       continue;
 
       if (color === '#0079D8') {
         // Ready for feedback
         if (diff > 0) {
           isAfterDependentStartDate = true;
-          // TODO: if gantItemm is also a prerequisite to another task, they should all recursively be checked and have warnings
-          // this.blockedDependents.set(ganttItem.id, true);
-        } else {
-          // this.blockedDependents.set(ganttItem.id, false);
         }
       } else if (color === '#31b0d5' || color === '#5BB75B') {
         // Discuss or Complete
         if (diff >= -7 * 24 * 60 * 60) {
           // We need to ensure this task is submitted a week earlier than its dependent so get it in a Discuss state
           isAfterDependentStartDate = true;
-          // this.blockedDependents.set(ganttItem.id, true);
-        } else {
-          // this.blockedDependents.set(ganttItem.id, false);
         }
       }
     }
-
-    if (this.blockedDependents.get(item.id) === true) {
-      return true;
-    }
-
-    // const prerequisites = this.items.filter((i) => {
-    //   const links = i.links;
-    //   if (typeof links === 'string') {
-    //     return false;
-    //   }
-
-    //   return links.some((l) => typeof l !== 'string' && l.link === i.id);
-    // });
 
     return isAfterDependentStartDate;
   }
 
   isBlockedByPrerequisite(item: GanttItem) {
-    // if (this.blockedDependents.get(item.id) === true) {
-    //   return true;
-    // }
-
     const prerequisites = this.items.filter((i) => {
       const links = i.links;
       if (typeof links === 'string') {
@@ -298,12 +233,6 @@ export class TaskPlannerComponent implements OnInit {
       return links.some((l) => typeof l !== 'string' && l.link === item.id);
     });
 
-    // const td = this.project.unit.taskDefinitions.find((td) => td.id === Number(item.id));
-
-    // if (td.abbreviation === 'D2') {
-    //   console.log(prerequisites);
-    // }
-
     for (const link of prerequisites) {
       if (this.prerequisiteConflict(link)) {
         const diff = this.normalizeDateUTC(link.end) - this.normalizeDateUTC(item.end);
@@ -311,13 +240,6 @@ export class TaskPlannerComponent implements OnInit {
           return true;
         }
       }
-    }
-
-    if (prerequisites.some((p) => this.blockedDependents.get(p.id) === true)) {
-      // this.blockedDependents.set(item.id, true);
-      return true;
-    } else {
-      // this.blockedDependents.set(item.id, false);
     }
 
     return false;
@@ -492,17 +414,6 @@ export class TaskPlannerComponent implements OnInit {
   }
 
   getTooltip(item: GanttItem) {
-    const prereqs = this.getPrerequisitesFor(item.id)
-      .map((p) => {
-        const td = this.taskDefs().find((t) => t.id === p.taskDefinitionId);
-        return `${td?.abbreviation}` || '';
-      })
-      .filter(Boolean);
-
-    // return `${this.toDateString(item.start)} - ${this.toDateString(item.end)}\n${
-    //   prereqs.length ? `Required for: \n${prereqs.join(', ')}` : ''
-    // }`;
-
     return `${this.toDateString(item.start)} — ${this.toDateString(item.end)}`;
   }
 
@@ -577,8 +488,6 @@ export class TaskPlannerComponent implements OnInit {
         title: `${td.abbreviation} ${td.name}`,
         start: this.normalizeDateUTC(task.startDate.getTime() / 1000),
         end: this.normalizeDateUTC(task.localDueDate().getTime() / 1000),
-        // start: Math.floor(task.startDate.getTime() / 1000),
-        // end: Math.floor(task.localDueDate().getTime() / 1000),
         expandable: false,
         draggable: this.project.unit.allowFlexibleDates,
         // color: this.gradeService.gradeColors[td.targetGrade],
@@ -594,14 +503,12 @@ export class TaskPlannerComponent implements OnInit {
             switch (p.taskStatus) {
               case 'ready_for_feedback':
                 color = 'rgba(0, 121, 216, 0.1)';
-                // color = '#90c8fc';
                 break;
               case 'complete':
                 color = 'rgba(91, 183, 91, 0.1)';
                 break;
               case 'discuss':
                 color = 'rgba(49, 176, 213, 0.1)';
-                // color = '#31b0d5';
                 break;
               case 'demonstrate':
                 color = 'rgba(49, 176, 213, 0.1)';
@@ -615,7 +522,6 @@ export class TaskPlannerComponent implements OnInit {
               // link: p.prerequisiteId.toString(),
               color: {
                 default: color,
-                // active: '#00000000',
                 active: color,
               },
             };
@@ -643,12 +549,6 @@ export class TaskPlannerComponent implements OnInit {
       // this.originalLinks.set(, [...(item.links as GanttLink[])]);
       const originalItem = {...item};
       this.originalLinks.set(item.id.toString(), [...(originalItem.links as GanttLink[])]);
-
-      // console.log(this.originalLinks);
-      // item.links.forEach((linkItem) => {
-      //   const link = linkItem as GanttLink;
-      //   (link.color as {active: string; default: string}).default += 'FC';
-      // });
 
       this.items.push(item);
       this.items = [...this.items];
