9f15954c7ce37f9aa65279c56b94c47d902a3c8b
b0ink <40929320+b0ink@users.noreply.github.com>
2026-01-12 14:16:47 +1100

    refactor: add confirmation modals

--- src/app/units/states/tasks/inbox/directives/moderation/moderation.component.ts
+++ src/app/units/states/tasks/inbox/directives/moderation/moderation.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Task} from 'src/app/api/models/task';
+import {ConfirmationModalService} from 'src/app/common/modals/confirmation-modal/confirmation-modal.service';
 import {AlertService} from 'src/app/common/services/alert.service';
 
 @Component({
@@ -12,40 +13,44 @@ export class ModerationComponent {
 
   public moderated: Map<Task, boolean> = new Map<Task, boolean>();
 
-  constructor(private alertService: AlertService) {}
+  constructor(
+    private alertService: AlertService,
+    private confirmationModal: ConfirmationModalService,
+  ) {}
 
   showMore() {
     console.log('bad!');
-
-    this.task.moderateFeedback(-1).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.alertService.success(`Task moderated`, 2000);
-        this.setModerated(this.task);
+    this.confirmationModal.show(
+      'Show more from this tutor',
+      `There were concerns with feedback and you would like to see more tasks from this tutor. You will review this task again if further feedback is provided.`,
+      () => {
+        this.moderateTask(-1);
       },
-      error: (error) => {
-        this.alertService.error(`Failed to moderate task: ${error}`, 6000);
-      },
-    });
+    );
   }
 
   showLess() {
-    console.log('good!');
-
-    this.task.moderateFeedback(1).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.alertService.success(`Task moderated`, 2000);
-        this.setModerated(this.task);
+    this.confirmationModal.show(
+      'Show less from this tutor',
+      `The feedback provided by the tutor is satisfactory and you would like to see less of their feedback for moderation. You won't see this task again.`,
+      () => {
+        this.moderateTask(1);
       },
-      error: (error) => {
-        this.alertService.error(`Failed to moderate task: ${error}`, 6000);
-      },
-    });
+    );
   }
 
   dismiss() {
-    this.task.moderateFeedback(0).subscribe({
+    this.confirmationModal.show(
+      'Dismiss from moderation',
+      'This task will be removed from moderation and will not get a follow up. This will not affect how many tasks from this tutor are selected for moderation.',
+      () => {
+        this.moderateTask(0);
+      },
+    );
+  }
+
+  private moderateTask(score: -1 | 0 | 1) {
+    this.task.moderateFeedback(score).subscribe({
       next: (response) => {
         console.log(response);
         this.alertService.success(`Task moderated`, 2000);
