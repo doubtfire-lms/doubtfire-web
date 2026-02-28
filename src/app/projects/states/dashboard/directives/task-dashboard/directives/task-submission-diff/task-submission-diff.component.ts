bda26a7f992e191dad86f28e05f2ca9a1a692669
b0ink <40929320+b0ink@users.noreply.github.com>
2026-02-28 11:55:43 +1100

    feat: submission history

---
+++ src/app/projects/states/dashboard/directives/task-dashboard/directives/task-submission-diff/task-submission-diff.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'f-task-submission-diff',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-submission-diff.component.html',
+  styleUrl: './task-submission-diff.component.scss'
+})
+export class TaskSubmissionDiffComponent {
+
+}
