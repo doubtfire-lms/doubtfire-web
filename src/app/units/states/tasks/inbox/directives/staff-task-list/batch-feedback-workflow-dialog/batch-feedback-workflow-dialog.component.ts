d96c22063dc8672cb3ee932c971616ea699a2253
b0ink <40929320+b0ink@users.noreply.github.com>
2026-04-18 17:17:23 +1000

    refactor: ensure single pdf submission to use as final pdf

--- src/app/units/states/tasks/inbox/directives/staff-task-list/batch-feedback-workflow-dialog/batch-feedback-workflow-dialog.component.ts
+++ src/app/units/states/tasks/inbox/directives/staff-task-list/batch-feedback-workflow-dialog/batch-feedback-workflow-dialog.component.ts
@@ -26,14 +26,6 @@ export class BatchFeedbackWorkflowDialogComponent {
     return `${this.data.taskDefinition.abbreviation} ${this.data.taskDefinition.name}`;
   }
 
-  get uploadRequirementNames(): string[] {
-    return (
-      this.data.taskDefinition?.uploadRequirements
-        ?.map((requirement) => requirement.name)
-        .filter((name): name is string => !!name) ?? []
-    );
-  }
-
   continueToUpload(): void {
     this.dialogRef.close({openUpload: true});
   }
