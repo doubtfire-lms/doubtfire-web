88a866ea7ff2e17be331ec2c09b7c2d67f4c667c
Cilly Leang <mini@cilly.moe>
2026-05-12 22:33:08 +1000

    feat(dashboard): create dashboard list item

--- src/app/dashboard/dashboard-list-item.component.ts
+++ src/app/dashboard/dashboard-list-item.component.ts
@@ -1,24 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, Input} from '@angular/core';
+
+export type DashboardTask = {
+  title: string;
+  subtitle: string;
+  abbreviation: string;
+  color: string;
+  comments: number;
+};
 
 @Component({
-  selector: 'dashboard-list-item',
+  selector: 'f-dashboard-list-item',
   templateUrl: './dashboard-list-item.component.html',
-  styleUrls: ['./dashboard-list-item.component.scss']
 })
 export class DashboardListItemComponent {
-  @Input() task: any;
-
-  constructor(private router: Router) {}
-
-  goToTask() {
-    if (!this.task) return;
-
-    this.router.navigate([
-      '/units',
-      this.task.unitId,
-      'tasks',
-      this.task.id
-    ]);
-  }
+  @Input() task: DashboardTask;
 }
