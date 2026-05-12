d19717cd5c2dea3bfca37e8645a93205155b82ee
Cilly Leang <mini@cilly.moe>
2026-05-12 22:33:44 +1000

    feat(dashboard): link up items to main dashboard page

--- src/app/dashboard/f-cross-dashboard.component.ts
+++ src/app/dashboard/f-cross-dashboard.component.ts
@@ -1,41 +1,57 @@
 import {Component, OnInit} from '@angular/core';
-import {Unit} from 'src/app/api/models/unit';
 import {GlobalStateService} from 'src/app/projects/states/index/global-state.service';
+import {Project} from '../api/models/project';
+import {TaskStatus} from '../api/models/task-status';
+import {DashboardTask} from './dashboard-list-item.component';
+import {Task} from '../api/models/task';
+import {TaskDefinition} from '../api/models/task-definition';
 
-type IUnits = {
-  id: number;
+type DashboardUnit = {
+  projectId: number;
   code: string;
   name: string;
-  // tasks: ITask[];
+  tasks: DashboardTask[];
 };
 
 @Component({
   selector: 'f-cross-dashboard',
   templateUrl: './f-cross-dashboard.component.html',
-  styleUrls: ['./f-cross-dashboard.component.scss'],
 })
 export class CrossDashboardComponent implements OnInit {
   constructor(private globalStateService: GlobalStateService) {}
 
-  units: IUnits[] = [];
+  units: DashboardUnit[] = [];
 
   ngOnInit(): void {
     this.globalStateService.onLoad(() => {
-      this.globalStateService.loadedUnits.values.subscribe((units) => {
-        this.units = this.mapUnits(units);
+      this.globalStateService.currentUserProjects.values.subscribe((projects) => {
+        this.units = this.mapProjects(projects);
       });
     });
   }
 
-  mapUnit(unit: Unit): IUnits {
-    return {
-      id: unit.id,
-      code: unit.code,
-      name: unit.name,
-    };
+  mapProjects(projects: readonly Project[]): DashboardUnit[] {
+    return projects.map((project) => {
+      const unit = project.unit;
+      return {
+        projectId: project.id,
+        code: unit.code,
+        name: unit.name,
+        tasks: this.mapTasks(project.tasks, unit.taskDefinitions),
+      };
+    });
   }
 
-  mapUnits(units: readonly Unit[]) {
-    return units.map((unit) => this.mapUnit(unit));
+  mapTasks(tasks: readonly Task[], taskDefs: readonly TaskDefinition[]): DashboardTask[] {
+    return taskDefs.map((def) => {
+      const task = tasks.find((t) => t.taskDefId == def.id);
+      return {
+        title: def.name,
+        subtitle: `${def.abbreviation} - ${def.targetGradeText} Task`,
+        abbreviation: def.abbreviation,
+        color: TaskStatus.STATUS_COLORS.get(task?.status ?? 'not_started'),
+        comments: task?.numNewComments ?? 0,
+      };
+    });
   }
 }
