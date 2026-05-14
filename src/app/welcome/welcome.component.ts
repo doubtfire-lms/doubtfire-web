21172a58c68b0e0a111c664ecf4a8d6c146bebdd
b0ink <40929320+b0ink@users.noreply.github.com>
2026-05-14 23:33:49 +1000

    chore: auto redirect to home if not first time setup

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,12 +3,13 @@ import {Router} from '@angular/router';
 import {AuthenticationService} from 'src/app/api/services/authentication.service';
 import {DoubtfireConstants} from 'src/app/config/constants/doubtfire-constants';
 import {GlobalStateService} from '../projects/states/index/global-state.service';
+import {UserService} from '../api/services/user.service';
 
 @Component({
-    selector: 'f-welcome',
-    templateUrl: './welcome.component.html',
-    styleUrls: ['./welcome.component.scss'],
-    standalone: false
+  selector: 'f-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
+  standalone: false,
 })
 export class WelcomeComponent implements OnInit {
   public loading = true;
@@ -18,6 +19,7 @@ export class WelcomeComponent implements OnInit {
     private globalState: GlobalStateService,
     private authenticationService: AuthenticationService,
     private router: Router,
+    private userService: UserService,
   ) {}
 
   public externalName = this.constants.ExternalName;
@@ -28,6 +30,10 @@ export class WelcomeComponent implements OnInit {
     this.authenticationService.afterAuthCall((result) => {
       if (!result) {
         return this.router.navigateByUrl('/sign_in');
+      }
+
+      if (this.userService.currentUser.hasRunFirstTimeSetup) {
+        return this.router.navigateByUrl('/home');
       }
 
       this.loading = false;
