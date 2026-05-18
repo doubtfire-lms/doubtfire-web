fd7bac6edcf384a28b101e820a70bef21b5482ab
Labibatanjim <labibatanjimrida@gmail.com>
2026-05-19 00:34:29 +1000

    fix: add class body and Input properties to component

--- src/app/dashboard/dashboard-list-item.component.ts
+++ src/app/dashboard/dashboard-list-item.component.ts
@@ -1,17 +1,24 @@
-import {Component, Input} from '@angular/core';
-
-export type DashboardTask = {
-  title: string;
-  subtitle: string;
-  abbreviation: string;
-  color: string;
-  comments: number;
-};
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'f-dashboard-list-item',
   templateUrl: './dashboard-list-item.component.html',
+  styleUrl: './dashboard-list-item.component.scss',
+  standalone: true,
+  imports: [CommonModule],
 })
 export class DashboardListItemComponent {
-  @Input() task: DashboardTask;
+  @Input() taskName: string = 'Task Name';
+  @Input() taskLabel: string = '1.1P - Pass Task';
+  @Input() color: string = 'gray';
+  @Input() alertType: string = '';
+  @Input() alertCount: number = 0;
+  @Input() badge: string = '';
+  @Input() badgeColor: string = 'gray';
+  @Input() time: string = '';
+  @Input() description: string = '';
+  @Input() dueDate: string = '';
+
+  expanded: boolean = false;
 }
