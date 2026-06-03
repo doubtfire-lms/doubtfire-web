1f906ba0e20a4313e95e41db2f076fb9169e2b31
b0ink <40929320+b0ink@users.noreply.github.com>
2026-06-03 11:06:08 +1000

    refactor: migrate font awesome icons to material icons

--- src/app/api/models/task.ts
+++ src/app/api/models/task.ts
@@ -525,6 +525,7 @@ export class Task extends Entity {
 
   public timeToDue(): string {
     const days = this.daysUntilDueDate();
+    // TODO: check with <=
     if (days < 0) {
       return 'Past Due Date';
     } else if (days < 11) {
@@ -705,7 +706,7 @@ export class Task extends Entity {
   }
 
   public statusIcon(): string {
-    return TaskStatus.STATUS_ICONS.get(this.status);
+    return TaskStatus.STATUS_MATERIAL_ICONS.get(this.status);
   }
 
   public statusClass(): string {
