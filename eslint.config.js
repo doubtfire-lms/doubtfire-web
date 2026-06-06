281f385cbda14135ae37eb59c3aed5287c4ee01a
b0ink <40929320+b0ink@users.noreply.github.com>
2026-06-06 18:49:07 +1000

    chore: enforce no empty content

--- eslint.config.js
+++ eslint.config.js
@@ -94,11 +94,10 @@ module.exports = tseslint.config(
     files: ['**/*.html'],
     rules: {
       '@angular-eslint/template/prefer-control-flow': 'error',
+      '@angular-eslint/template/label-has-associated-control': 'off',
       '@angular-eslint/template/mouse-events-have-key-events': 'off',
       '@angular-eslint/template/click-events-have-key-events': 'off',
       '@angular-eslint/template/interactive-supports-focus': 'off',
-      '@angular-eslint/template/elements-content': 'off',
-      '@angular-eslint/template/label-has-associated-control': 'off',
     },
   },
 );
