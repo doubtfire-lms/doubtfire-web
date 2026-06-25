2d24ebc9e611dbf7ca0cbdcd8507b238538d9ea3
b0ink <40929320+b0ink@users.noreply.github.com>
2026-06-25 12:59:43 +1000

    fix: tunnel sentry requests

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {DoubtfireAngularModule} from './app/doubtfire-angular.module';
 if (environment.sentryDsn) {
   Sentry.init({
     dsn: environment.sentryDsn,
+    tunnel: '/api/sentry/tunnel',
     release: environment.sentryRelease || undefined,
     dist: environment.sentryDist || undefined,
     integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
