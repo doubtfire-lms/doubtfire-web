bcaaa4d97bee8f04b7bf24d011ab45f8ebea234e
b0ink <40929320+b0ink@users.noreply.github.com>
2026-07-29 16:28:23 +1000

    fix: ensure rediscuss unlocks submission with prereqs

--- src/app/units/states/edit/directives/unit-tasks-editor/task-definition-editor/task-definition-prerequisites/task-definition-prerequisites.component.ts
+++ src/app/units/states/edit/directives/unit-tasks-editor/task-definition-editor/task-definition-prerequisites/task-definition-prerequisites.component.ts
@@ -46,6 +46,7 @@ export class TaskDefinitionPrerequisitesComponent implements OnInit, OnChanges {
     ready_for_feedback: 1,
     assess_in_portfolio: 1,
     discuss: 2,
+    rediscuss: 2,
     attention_required: 0,
     demonstrate: 2,
     complete: 3,
