c5f0a7ec398be016199c3d28a648e24db3d2bbe8
Audrey Phommasone <audreypho@outlook.com>
2026-09-08 13:53:31 +1000

    fix: chart title names & description

--- src/app/units/states/analytics/directives/charts/stacked-area-status-chart/stacked-area-status-chart.component.ts
+++ src/app/units/states/analytics/directives/charts/stacked-area-status-chart/stacked-area-status-chart.component.ts
@@ -1,5 +1,7 @@
-import {MultiSeries} from '@glitchtip/ng-charts';
-import {Component, Input} from '@angular/core';
+import {MultiSeries, TooltipService} from '@glitchtip/ng-charts';
+import {ChangeDetectorRef, Component, Injector, Input, ViewContainerRef} from '@angular/core';
+import {TaskService} from 'src/app/api/services/task.service';
+import {AlertService} from 'src/app/common/services/alert.service';
 
 @Component({
   selector: 'f-stacked-area-status-chart',
@@ -13,4 +15,17 @@ export class StackedAreaStatusChartComponent {
 
   XAxisLabel: string = 'Week';
   YAxisLabel: string = 'Records';
+
+  constructor(
+    private taskService: TaskService,
+    private alertService: AlertService,
+    private chartToolTipService: TooltipService,
+    private viewContainerRef: ViewContainerRef,
+    private injectorObj: Injector,
+    private changeDetectorRef: ChangeDetectorRef,
+  ) {
+    // https://github.com/swimlane/ngx-charts/issues/1428#issuecomment-659237562
+    this.chartToolTipService = this.injectorObj.get(TooltipService);
+    this.viewContainerRef = this.injectorObj.get(ViewContainerRef);
+  }
 }
