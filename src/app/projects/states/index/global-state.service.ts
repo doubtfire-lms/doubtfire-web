6dc1b14fc97e6b9b863c0cfabf2ceb0f4cd613a8
baohuy-tran <baohuy.tranaus@gmail.com>
2026-09-13 21:38:43 +1000

    refactor: replace MediaObserver in global state service

--- src/app/projects/states/index/global-state.service.ts
+++ src/app/projects/states/index/global-state.service.ts
@@ -1,5 +1,5 @@
-import {MediaObserver} from 'ng-flex-layout';
 import {EntityCache} from 'ngx-entity-service';
+import {BreakpointObserver} from '@angular/cdk/layout';
 import {Injectable, OnDestroy} from '@angular/core';
 import {Router} from '@angular/router';
 import {BehaviorSubject, Observable, Subject, find} from 'rxjs';
@@ -113,7 +113,7 @@ export class GlobalStateService implements OnDestroy {
     private feedbackTemplateService: FeedbackTemplateService,
     private router: Router,
     private alerts: AlertService,
-    private mediaObserver: MediaObserver,
+    private breakpointObserver: BreakpointObserver,
   ) {
     this.loadedUnitRoles = this.unitRoleService.cache;
     this.loadedUnits = this.unitService.cache;
@@ -156,7 +156,7 @@ export class GlobalStateService implements OnDestroy {
 
       if (this._isInboxState) {
         document.body.style.setProperty('--vh', `${vh}px`);
-      } else if (!this.mediaObserver.isActive('gt-sm') || !this._showFooter) {
+      } else if (!this.breakpointObserver.isMatched('(min-width: 960px)') || !this._showFooter) {
         document.body.style.setProperty('--vh', `${vh - 0.2}px`);
       } else {
         if (this._showFooter && !this._showFooterWarning) {
