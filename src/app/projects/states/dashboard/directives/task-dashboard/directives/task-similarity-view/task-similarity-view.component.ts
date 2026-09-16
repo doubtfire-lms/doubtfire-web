76f1ce31dc6a033eec7373d0c03dc436250f9c9b
b0ink <40929320+b0ink@users.noreply.github.com>
2026-09-16 20:27:39 +1000

    feat: show loading spinners for similarities and jplag reports

--- src/app/projects/states/dashboard/directives/task-dashboard/directives/task-similarity-view/task-similarity-view.component.ts
+++ src/app/projects/states/dashboard/directives/task-dashboard/directives/task-similarity-view/task-similarity-view.component.ts
@@ -7,6 +7,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import {MatAccordion} from '@angular/material/expansion';
+import {finalize} from 'rxjs';
 import {Task} from 'src/app/api/models/task';
 import {TaskSimilarity} from 'src/app/api/models/task-similarity';
 import {TaskSimilarityService} from 'src/app/api/services/task-similarity.service';
@@ -28,6 +29,8 @@ export class TaskSimilarityViewComponent implements OnChanges {
   @ViewChild('jplagViewer') jplagViewer!: JplagReportViewerComponent;
   panelOpenState = false;
   jplagOpenState = false;
+  loadingSimilarities = false;
+  loadingJplagReportFor: TaskSimilarity | null = null;
 
   constructor(
     private taskSimilarityService: TaskSimilarityService,
@@ -39,7 +42,11 @@ export class TaskSimilarityViewComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     if (changes.task && changes.task.currentValue && this.task?.id) {
       this.jplagOpenState = false;
-      this.task?.fetchSimilarities().subscribe();
+      this.loadingSimilarities = true;
+      this.task
+        ?.fetchSimilarities()
+        .pipe(finalize(() => (this.loadingSimilarities = false)))
+        .subscribe();
     }
   }
 
@@ -75,6 +82,7 @@ export class TaskSimilarityViewComponent implements OnChanges {
   viewJplagReport(similarity: TaskSimilarity) {
     // Students are identified by their username in JPlag reports (configured by API)
     // In most cases, usernames are a combination of their first and last names
+    this.loadingJplagReportFor = similarity;
     this.fileDownloaderService.downloadBlob(
       this.task.definition.getJplagReportUrl(),
       (url: string) => {
@@ -89,10 +97,12 @@ export class TaskSimilarityViewComponent implements OnChanges {
               similarity.otherStudent.username,
             );
             this.jplagOpenState = true;
+            this.loadingJplagReportFor = null;
           });
       },
       (error) => {
         console.error(error);
+        this.loadingJplagReportFor = null;
       },
     );
   }
