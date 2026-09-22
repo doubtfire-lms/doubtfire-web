27b5d14545f9180e7c55148504736616025d885b
Audrey Phommasone <audreypho@outlook.com>
2026-09-22 15:32:27 +1000

    fix: linting

--- src/app/units/states/analytics/directives/charts/chart-data-helpers.ts
+++ src/app/units/states/analytics/directives/charts/chart-data-helpers.ts
@@ -127,9 +127,7 @@ export function getTaskStats(
         }, {} as CampusStats);
 
   const campusStats =
-  campusFilter === 'all'
-    ? snapshotStats
-    : {[campusFilter]: snapshotStats[campusFilter] ?? {}};
+    campusFilter === 'all' ? snapshotStats : {[campusFilter]: snapshotStats[campusFilter] ?? {}};
 
   const filteredStats = Object.values(campusStats).reduce((acc, tutorials) => {
     const selectedTutorials =
